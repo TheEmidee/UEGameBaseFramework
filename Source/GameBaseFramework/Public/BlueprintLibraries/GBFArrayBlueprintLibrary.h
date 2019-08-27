a4a64b0be7788f3e832e7cf0df136d766ae332b9
Michael Delva <michael.delva@fishingcactus.com>
2019-08-27 19:13:25 +0200

    Added GBFArrayBlueprintLibrary

---
+++ Source/GameBaseFramework/Public/BlueprintLibraries/GBFArrayBlueprintLibrary.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <CoreMinimal.h>
+#include <Kismet/BlueprintFunctionLibrary.h>
+
+#include "GBFArrayBlueprintLibrary.generated.h"
+
+UCLASS()
+class GAMEBASEFRAMEWORK_API UGBFArrayBlueprintLibrary : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+
+    template < typename _ARRAY_TYPE_ >
+    static void ShuffleArray( _ARRAY_TYPE_ & array, const FRandomStream & random_stream )
+    {
+        for ( int index = array.Num() - 1; index > 0; index-- )
+        {
+            int new_index = random_stream.RandRange( 0, index + 1 );
+            Swap( array[ index ], array[ new_index ] );
+        }
+    }
+
+    template < typename _ENUM_TYPE_ >
+    static TArray< _ENUM_TYPE_, TInlineAllocator< static_cast< int32 >( _ENUM_TYPE_::Count ) > > GetInlinedArrayFromEnum()
+    {
+        TArray< _ENUM_TYPE_, TInlineAllocator< static_cast< int32 >( _ENUM_TYPE_::Count ) > > array;
+        array.Reserve( static_cast< int32 >( _ENUM_TYPE_::Count ) );
+
+        for ( int index = 0; index < static_cast< int32 >( _ENUM_TYPE_::Count ); index++ )
+        {
+            array.Add( static_cast< _ENUM_TYPE_ >( index ) );
+        }
+
+        return array;
+    }
+
+    template < typename _ENUM_TYPE_ >
+    static TArray< _ENUM_TYPE_, TInlineAllocator< static_cast< int32 >( _ENUM_TYPE_::Count ) > > GetShuffledInlinedArrayFromEnum( const FRandomStream & random_stream )
+    {
+        auto array = GetInlinedArrayFromEnum< _ENUM_TYPE_ >();
+        ShuffleArray( array, random_stream );
+        return array;
+    }
+};
