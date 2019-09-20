24193237d52402c558f0a3bd843a95de62983beb
Michael Delva <michael.delva@fishingcactus.com>
2019-09-20 19:43:59 +0200

    default copy and move constructors for FEdge2D

--- Source/GameBaseFramework/Public/Math/Edge2D.h
+++ Source/GameBaseFramework/Public/Math/Edge2D.h
@@ -5,10 +5,16 @@
 struct GAMEBASEFRAMEWORK_API FEdge2D
 {
     FEdge2D() = default;
-    FEdge2D( const FVector2D & from, const FVector2D & to );
+    FEdge2D( const FEdge2D & other ) = default;
+    FEdge2D( FEdge2D && other ) = default;
+    FEdge2D & operator=( const FEdge2D & other ) = default;
+    FEdge2D & operator=( FEdge2D && other ) = default;
+
 
     bool operator==( const FEdge2D & other ) const;
     bool operator!=( const FEdge2D & other ) const;
+
+    FEdge2D( const FVector2D & from, const FVector2D & to );
 
     float Size() const;
     float SizeSquared() const;
