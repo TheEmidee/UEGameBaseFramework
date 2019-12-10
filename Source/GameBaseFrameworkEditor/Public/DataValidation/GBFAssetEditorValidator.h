d7f88b9dbd33f81803db3def39102e6f1de8d7ba
Michael Delva <michael.delva@fishingcactus.com>
2019-12-10 16:40:02 +0100

    Added new constructor to GBFAssetEditorValidator to allow folder filtering

--- Source/GameBaseFrameworkEditor/Public/DataValidation/GBFAssetEditorValidator.h
+++ Source/GameBaseFrameworkEditor/Public/DataValidation/GBFAssetEditorValidator.h
@@ -2,16 +2,21 @@
 
 #include "Path/GBFPathFilter.h"
 
-#include <CoreMinimal.h>
-
 template < typename ASSET_TYPE >
 struct FGBFAssetEditorValidator
 {
-public:
     template < typename FILENAME_FILTER_BUILDER_TYPE >
-    explicit FGBFAssetEditorValidator( FILENAME_FILTER_BUILDER_TYPE && filename_filter_builder )
+    explicit FGBFAssetEditorValidator( FILENAME_FILTER_BUILDER_TYPE && filename_filter_builder ) :
+        FileNameFilter { filename_filter_builder() }
     {
-        FileNameFilter = filename_filter_builder();
+    }
+
+    template < typename FILENAME_FILTER_BUILDER_TYPE, typename FOLDER_FILTER_BUILDER_TYPE >
+    explicit FGBFAssetEditorValidator( FILENAME_FILTER_BUILDER_TYPE && filename_filter_builder, FOLDER_FILTER_BUILDER_TYPE && folder_filter_builder ) :
+        FileNameFilter { filename_filter_builder() },
+        FolderFilter { folder_filter_builder() }
+    {
+        
     }
 
     bool IsValid( UObject * object ) const
@@ -21,9 +26,11 @@ public:
             return false;
         }
 
-        return FileNameFilter.Matches( object->GetName() );
+        return FolderFilter.Matches( object->GetPathName() )
+            && FileNameFilter.Matches( object->GetName() );
     }
 
 private:
     FGBFPathFilter FileNameFilter;
+    FGBFPathFilter FolderFilter;
 };
