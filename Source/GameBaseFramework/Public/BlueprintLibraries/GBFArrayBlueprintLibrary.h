9b9ab8815301c96b2168dbba5318c3d0d0d1cc78
Michael Delva <michael@emidee.net>
2019-12-26 16:12:04 +0100

    Removed _ character as prefix and suffix for template parameters

--- Source/GameBaseFramework/Public/BlueprintLibraries/GBFArrayBlueprintLibrary.h
+++ Source/GameBaseFramework/Public/BlueprintLibraries/GBFArrayBlueprintLibrary.h
@@ -11,8 +11,8 @@ class GAMEBASEFRAMEWORK_API UGBFArrayBlueprintLibrary : public UBlueprintFunctio
     GENERATED_BODY()
 
 public:
-    template < typename _ARRAY_TYPE_ >
-    static void ShuffleArray( _ARRAY_TYPE_ & array, const FRandomStream & random_stream )
+    template < typename ARRAY_TYPE_ >
+    static void ShuffleArray( ARRAY_TYPE_ & array, const FRandomStream & random_stream )
     {
         for ( int index = array.Num() - 1; index > 0; --index )
         {
@@ -21,30 +21,30 @@ public:
         }
     }
 
-    template < typename _ENUM_TYPE_ >
-    static TArray< _ENUM_TYPE_, TInlineAllocator< static_cast< int32 >( _ENUM_TYPE_::Count ) > > GetInlinedArrayFromEnum()
+    template < typename ENUM_TYPE_ >
+    static TArray< ENUM_TYPE_, TInlineAllocator< static_cast< int32 >( ENUM_TYPE_::Count ) > > GetInlinedArrayFromEnum()
     {
-        TArray< _ENUM_TYPE_, TInlineAllocator< static_cast< int32 >( _ENUM_TYPE_::Count ) > > array;
-        array.Reserve( static_cast< int32 >( _ENUM_TYPE_::Count ) );
+        TArray< ENUM_TYPE_, TInlineAllocator< static_cast< int32 >( ENUM_TYPE_::Count ) > > array;
+        array.Reserve( static_cast< int32 >( ENUM_TYPE_::Count ) );
 
-        for ( auto index = 0; index < static_cast< int32 >( _ENUM_TYPE_::Count ); index++ )
+        for ( auto index = 0; index < static_cast< int32 >( ENUM_TYPE_::Count ); index++ )
         {
-            array.Add( static_cast< _ENUM_TYPE_ >( index ) );
+            array.Add( static_cast< ENUM_TYPE_ >( index ) );
         }
 
         return array;
     }
 
-    template < typename _ENUM_TYPE_ >
-    static TArray< _ENUM_TYPE_, TInlineAllocator< static_cast< int32 >( _ENUM_TYPE_::Count ) > > GetShuffledInlinedArrayFromEnum( const FRandomStream & random_stream )
+    template < typename ENUM_TYPE_ >
+    static TArray< ENUM_TYPE_, TInlineAllocator< static_cast< int32 >( ENUM_TYPE_::Count ) > > GetShuffledInlinedArrayFromEnum( const FRandomStream & random_stream )
     {
-        auto array = GetInlinedArrayFromEnum< _ENUM_TYPE_ >();
+        auto array = GetInlinedArrayFromEnum< ENUM_TYPE_ >();
         ShuffleArray( array, random_stream );
         return array;
     }
 
-    template < typename _ARRAY_TYPE_, typename _ARRAY_ALLOCATOR_TYPE_ >
-    static _ARRAY_TYPE_ GetRandomArrayValue( const TArray< _ARRAY_TYPE_, _ARRAY_ALLOCATOR_TYPE_ > & array, const FRandomStream & random_stream )
+    template < typename ARRAY_TYPE_, typename ARRAY_ALLOCATOR_TYPE_ >
+    static ARRAY_TYPE_ GetRandomArrayValue( const TArray< ARRAY_TYPE_, ARRAY_ALLOCATOR_TYPE_ > & array, const FRandomStream & random_stream )
     {
         return array[ random_stream.RandRange( 0, array.Num() - 1 ) ];
     }
