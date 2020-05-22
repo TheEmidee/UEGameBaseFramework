924fc8ca989977e7e3a88efba76e12dc67a85d95
Michael Delva <michael@emidee.net>
2020-05-22 12:20:02 +0200

    review

--- Source/GameBaseFramework/Private/GameFramework/GBFGameModeLogin.cpp
+++ Source/GameBaseFramework/Private/GameFramework/GBFGameModeLogin.cpp
@@ -20,7 +20,7 @@ void AGBFGameModeLogin::InitializeLocalPlayer( const int controller_index )
 {
     const auto & local_players = GetGameInstance()->GetLocalPlayers();
 
-    for ( auto index = local_players.Num() - 1; index >= 0; index-- )
+    for ( auto index = local_players.Num() - 1; index >= 0; --index )
     {
         auto * local_player = local_players[ index ];
         if ( local_player->GetControllerId() != controller_index )
