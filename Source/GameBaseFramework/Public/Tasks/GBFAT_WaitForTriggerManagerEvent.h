881628661d4f0601791ede8372f60748a189d232
Viktor Blomme <viktor.blomme@fishingcactus.com>
2022-04-01 11:01:20 +0200

    Rename

--- Source/GameBaseFramework/Public/Tasks/GBFAT_WaitForTriggerManagerEvent.h
+++ Source/GameBaseFramework/Public/Tasks/GBFAT_WaitForTriggerManagerEvent.h
@@ -2,20 +2,20 @@
 
 #include <Abilities/Tasks/AbilityTask.h>
 
-#include "GBFAT_MonitorTriggerManager.generated.h"
+#include "GBFAT_WaitForTriggerManagerEvent.generated.h"
 
 class UGBFTriggerManagerComponent;
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams( FGASExtOnMonitorTriggerManagerDelegate, AActor *, activator, int, actor_count );
 
 UCLASS()
-class GAMEBASEFRAMEWORK_API UGBFAT_MonitorTriggerManager : public UAbilityTask
+class GAMEBASEFRAMEWORK_API UGBFAT_WaitForTriggerManagerEvent : public UAbilityTask
 {
     GENERATED_BODY()
 
 public:
     UFUNCTION( BlueprintCallable, Category = "Ability|Tasks", meta = ( HidePin = "owning_ability", DefaultToSelf = "owning_ability", BlueprintInternalUseOnly = "TRUE" ) )
-    static UGBFAT_MonitorTriggerManager * MonitorTriggerManager( UGameplayAbility * owning_ability, UGBFTriggerManagerComponent * trigger_manager_component );
+    static UGBFAT_WaitForTriggerManagerEvent * WaitForTriggerManagerEvent( UGameplayAbility * owning_ability, UGBFTriggerManagerComponent * trigger_manager_component );
 
     void Activate() override;
     void OnDestroy( bool bInOwnerFinished ) override;
