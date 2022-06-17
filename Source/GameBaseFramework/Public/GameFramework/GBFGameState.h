3c7f2731f7758ecad3a6046863878db5c9efae9c
Michael Delva <michael.delva@fishingcactus.com>
2022-06-17 17:36:18 +0200

    Implemented GBFGameState

--- Source/GameBaseFramework/Public/GameFramework/GBFGameState.h
+++ Source/GameBaseFramework/Public/GameFramework/GBFGameState.h
@@ -2,12 +2,30 @@
 
 #include "ModularGameState.h"
 
+#include <AbilitySystemInterface.h>
 #include <CoreMinimal.h>
 
 #include "GBFGameState.generated.h"
 
+class UGASExtAbilitySystemComponent;
 UCLASS()
-class GAMEBASEFRAMEWORK_API AGBFGameState : public AModularGameState
+class GAMEBASEFRAMEWORK_API AGBFGameState : public AModularGameState, public IAbilitySystemInterface
 {
     GENERATED_BODY()
+
+public:
+
+    AGBFGameState();
+
+    UAbilitySystemComponent * GetAbilitySystemComponent() const override;
+    void PostInitializeComponents() override;
+    void Tick( float DeltaSeconds ) override;
+
+private:
+
+    UPROPERTY( VisibleAnywhere )
+    UGASExtAbilitySystemComponent * AbilitySystemComponent;
+
+    UPROPERTY( Replicated )
+    float ServerFPS;
 };
