3b060e77efb36b8e1ff94afead08a672e997b6fb
michael.delva <michael@emidee.net>
2022-06-21 14:00:57 +0200

    Temp

--- Source/GameBaseFramework/Private/Characters/GBFCharacter.cpp
+++ Source/GameBaseFramework/Private/Characters/GBFCharacter.cpp
@@ -1,5 +1,8 @@
 #include "Characters/GBFCharacter.h"
 
+#include "Characters/Components/GBFHealthComponent.h"
+#include "Characters/Components/GBFPawnExtensionComponent.h"
+
 AGBFCharacter::AGBFCharacter()
 {
     // Avoid ticking characters if possible.
@@ -7,4 +10,21 @@ AGBFCharacter::AGBFCharacter()
     PrimaryActorTick.bStartWithTickEnabled = false;
 
     NetCullDistanceSquared = 900000000.0f;
+
+    PawnExtComponent = CreateDefaultSubobject< UGBFPawnExtensionComponent >( TEXT( "PawnExtensionComponent" ) );
+    PawnExtComponent->OnAbilitySystemInitialized_RegisterAndCall( FSimpleMulticastDelegate::FDelegate::CreateUObject( this, &ThisClass::OnAbilitySystemInitialized ) );
+    PawnExtComponent->OnAbilitySystemUninitialized_Register( FSimpleMulticastDelegate::FDelegate::CreateUObject( this, &ThisClass::OnAbilitySystemUninitialized ) );
+
+    HealthComponent = CreateDefaultSubobject< UGBFHealthComponent >( TEXT( "HealthComponent" ) );
+    HealthComponent->OnDeathStarted.AddDynamic( this, &ThisClass::OnDeathStarted );
+    HealthComponent->OnDeathFinished.AddDynamic( this, &ThisClass::OnDeathFinished );
+}
+
+void AGBFCharacter::Reset()
+{
+    DisableMovementAndCollision();
+
+    K2_OnReset();
+
+    UninitAndDestroy();
 }
--- Source/GameBaseFramework/Public/Characters/GBFCharacter.h
+++ Source/GameBaseFramework/Public/Characters/GBFCharacter.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "Components/GBFPawnExtensionComponent.h"
 #include "ModularCharacter.h"
 
 #include <AbilitySystemInterface.h>
@@ -9,16 +10,17 @@
 
 #include "GBFCharacter.generated.h"
 
+class UGBFHealthComponent;
 class UGASExtAbilitySystemComponent;
 class AGBFPlayerState;
 
 UCLASS()
-class GAMEBASEFRAMEWORK_API AGBFCharacter : public AModularCharacter/*, public IAbilitySystemInterface, public IGameplayCueInterface, public IGameplayTagAssetInterface*/
+class GAMEBASEFRAMEWORK_API AGBFCharacter : public AModularCharacter /*, public IAbilitySystemInterface, public IGameplayCueInterface, public IGameplayTagAssetInterface*/
 {
     GENERATED_BODY()
 
 public:
-
+    
     AGBFCharacter();
 
     /*UFUNCTION( BlueprintCallable, Category = "Lyra|Character" )
@@ -32,6 +34,23 @@ public:
     bool HasMatchingGameplayTag( FGameplayTag TagToCheck ) const override;
     bool HasAllMatchingGameplayTags( const FGameplayTagContainer & TagContainer ) const override;
     bool HasAnyMatchingGameplayTags( const FGameplayTagContainer & TagContainer ) const override;
+    */
+    void Reset() override;
 
-    void Reset() override;*/
+protected:
+
+    virtual void OnAbilitySystemInitialized();
+    virtual void OnAbilitySystemUninitialized();
+
+    UFUNCTION()
+    virtual void OnDeathStarted( AActor * owning_actor );
+
+    UFUNCTION()
+    virtual void OnDeathFinished( AActor * owning_actor );
+
+    UPROPERTY( VisibleAnywhere, BlueprintReadOnly, Category = "Character", Meta = ( AllowPrivateAccess = "true" ) )
+    UGBFPawnExtensionComponent * PawnExtComponent;
+
+    UPROPERTY( VisibleAnywhere, BlueprintReadOnly, Category = "Character", Meta = ( AllowPrivateAccess = "true" ) )
+    UGBFHealthComponent * HealthComponent;
 };
