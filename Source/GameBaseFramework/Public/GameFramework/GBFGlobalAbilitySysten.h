25eacf9a1760ee4bca906657b0ec477fe138d3ff
michael.delva <michael@emidee.net>
2022-06-30 12:23:25 +0200

    Added global ability system

---
+++ Source/GameBaseFramework/Public/GameFramework/GBFGlobalAbilitySysten.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <CoreMinimal.h>
+#include <Subsystems/WorldSubsystem.h>
+
+#include "GBFGlobalAbilitySysten.generated.h"
+
+/**
+ * Imported from Lyra
+ */
+UCLASS()
+class GAMEBASEFRAMEWORK_API UGBFGlobalAbilitySysten final : public UWorldSubsystem
+{
+    GENERATED_BODY()
+};
