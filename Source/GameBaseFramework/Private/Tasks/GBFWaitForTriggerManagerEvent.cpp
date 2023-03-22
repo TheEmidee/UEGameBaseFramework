299699fc99a9bc2d360393f719ab07c2e4acab3d
Viktor Blomme <viktor.blomme@hotmail.com>
2023-03-22 13:36:40 +0100

    Create async task for trigger manager events

---
+++ Source/GameBaseFramework/Private/Tasks/GBFWaitForTriggerManagerEvent.cpp
@@ -0,0 +1,69 @@
+﻿#include "Tasks/GBFWaitForTriggerManagerEvent.h"
+
+#include "Gameplay/Components/GBFTriggerManagerComponent.h"
+
+UGBFWaitForTriggerManagerEvent * UGBFWaitForTriggerManagerEvent::WaitForTriggerManagerEvent( UObject * world_context_object,
+    UGBFTriggerManagerComponent * trigger_manager_component,
+    const bool broadcast_trigger_count_on_activate,
+    const bool trigger_once )
+{
+    auto * my_obj = NewObject< UGBFWaitForTriggerManagerEvent >();
+    my_obj->TriggerManagerComponent = trigger_manager_component;
+    my_obj->bBroadcastTriggerCountOnActivate = broadcast_trigger_count_on_activate;
+    my_obj->bTriggerOnce = trigger_once;
+    return my_obj;
+}
+
+void UGBFWaitForTriggerManagerEvent::Activate()
+{
+    Super::Activate();
+
+    if ( TriggerManagerComponent != nullptr )
+    {
+        TriggerManagerComponent->OnTriggerBoxActivated().AddDynamic( this, &ThisClass::OnTriggerActivated );
+        TriggerManagerComponent->OnActorInsideTriggerCountChanged().AddDynamic( this, &ThisClass::OnActorInsideTriggerCountChanged );
+
+        if ( bBroadcastTriggerCountOnActivate )
+        {
+            const auto & actors_in_trigger = TriggerManagerComponent->GetActorsInTrigger();
+            const auto actor_count = actors_in_trigger.Num();
+            auto * last_actor = actor_count > 0 ? actors_in_trigger.Last() : nullptr;
+
+            OnActorInsideTriggerCountChangedDelegate.Broadcast( last_actor, actor_count );
+        }
+    }
+}
+
+void UGBFWaitForTriggerManagerEvent::SetReadyToDestroy()
+{
+    if ( TriggerManagerComponent != nullptr )
+    {
+        TriggerManagerComponent->OnTriggerBoxActivated().RemoveDynamic( this, &ThisClass::OnTriggerActivated );
+        TriggerManagerComponent->OnActorInsideTriggerCountChanged().RemoveDynamic( this, &ThisClass::OnActorInsideTriggerCountChanged );
+    }
+
+    Super::SetReadyToDestroy();
+}
+
+void UGBFWaitForTriggerManagerEvent::OnTriggerActivated( AActor * activator )
+{
+
+    OnTriggerActivatedDelegate.Broadcast( activator, TriggerManagerComponent->GetActorsInTrigger().Num() );
+    CheckShouldEndTask();
+}
+
+void UGBFWaitForTriggerManagerEvent::OnActorInsideTriggerCountChanged( const int actor_count )
+{
+    const auto & actors_in_trigger = TriggerManagerComponent->GetActorsInTrigger();
+    auto * last_actor = actors_in_trigger.Num() > 0 ? actors_in_trigger.Last() : nullptr;
+
+    OnActorInsideTriggerCountChangedDelegate.Broadcast( last_actor, actor_count );
+}
+
+void UGBFWaitForTriggerManagerEvent::CheckShouldEndTask()
+{
+    if ( bTriggerOnce )
+    {
+        SetReadyToDestroy();
+    }
+}
---
+++ Source/GameBaseFramework/Public/Tasks/GBFWaitForTriggerManagerEvent.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+
+#include <Kismet/BlueprintAsyncActionBase.h>
+
+#include "GBFWaitForTriggerManagerEvent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams( FGBFOnWaitTriggerManagerEventDelegate, AActor *, activator, int, actor_count );
+
+UCLASS()
+class GAMEBASEFRAMEWORK_API UGBFWaitForTriggerManagerEvent final : public UBlueprintAsyncActionBase
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION( BlueprintCallable, meta = ( BlueprintInternalUseOnly = true, WorldContext = "world_context_object" ) )
+    static UGBFWaitForTriggerManagerEvent * WaitForTriggerManagerEvent( UObject * world_context_object,
+        UGBFTriggerManagerComponent * trigger_manager_component,
+        bool broadcast_trigger_count_on_activate,
+        bool trigger_once = false );
+
+    void Activate() override;
+    void SetReadyToDestroy() override;
+
+private:
+    UFUNCTION()
+    void OnTriggerActivated( AActor * activator );
+
+    UFUNCTION()
+    void OnActorInsideTriggerCountChanged( int actor_count );
+
+    void CheckShouldEndTask();
+
+    UPROPERTY( BlueprintAssignable )
+    FGBFOnWaitTriggerManagerEventDelegate OnTriggerActivatedDelegate;
+
+    UPROPERTY( BlueprintAssignable )
+    FGBFOnWaitTriggerManagerEventDelegate OnActorInsideTriggerCountChangedDelegate;
+
+    UPROPERTY()
+    UGBFTriggerManagerComponent * TriggerManagerComponent;
+
+    UPROPERTY()
+    uint8 bBroadcastTriggerCountOnActivate : 1;
+
+    UPROPERTY()
+    uint8 bTriggerOnce : 1;
+};
