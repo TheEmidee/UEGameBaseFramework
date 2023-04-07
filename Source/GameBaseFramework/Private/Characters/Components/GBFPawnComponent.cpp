c23bb86737545cf02586fc481189844ca118d59a
Michael Delva <michael.delva@fishingcactus.com>
2023-04-07 17:47:58 +0200

    Updated GBFPawnComponent to inherit from IGameFrameworkInitStateInterface with default implementation for common functions

--- Source/GameBaseFramework/Private/Characters/Components/GBFPawnComponent.cpp
+++ Source/GameBaseFramework/Private/Characters/Components/GBFPawnComponent.cpp
@@ -1,6 +1,48 @@
 #include "Characters/Components/GBFPawnComponent.h"
 
+#include "GBFTags.h"
+
 bool UGBFPawnComponent::IsPawnComponentReadyToInitialize() const
 {
-	return true;
+    // :TODO: Remove this function
+    ensure( false );
+    return true;
+}
+
+void UGBFPawnComponent::CheckDefaultInitialization()
+{
+    // Before checking our progress, try progressing any other features we might depend on
+    CheckDefaultInitializationForImplementers();
+
+    static const TArray< FGameplayTag > StateChain = { GBFTag_InitState_Spawned, GBFTag_InitState_DataAvailable, GBFTag_InitState_DataInitialized, GBFTag_InitState_GameplayReady };
+
+    // This will try to progress from spawned (which is only set in BeginPlay) through the data initialization stages until it gets to gameplay ready
+    ContinueInitStateChain( StateChain );
+}
+
+void UGBFPawnComponent::OnRegister()
+{
+    Super::OnRegister();
+
+    // Register with the init state system early, this will only work if this is a game world
+    RegisterInitStateFeature();
+}
+
+void UGBFPawnComponent::BeginPlay()
+{
+    Super::BeginPlay();
+
+    // Listen for changes to all features
+    BindOnActorInitStateChanged( GetFeatureName(), FGameplayTag(), false );
+
+    // Notifies state manager that we have spawned, then try rest of default initialization
+    ensure( TryToChangeInitState( GBFTag_InitState_Spawned ) );
+    CheckDefaultInitialization();
+}
+
+void UGBFPawnComponent::EndPlay( const EEndPlayReason::Type end_play_reason )
+{
+    UnregisterInitStateFeature();
+
+    Super::EndPlay( end_play_reason );
 }
--- Source/GameBaseFramework/Public/Characters/Components/GBFPawnComponent.h
+++ Source/GameBaseFramework/Public/Characters/Components/GBFPawnComponent.h
@@ -1,18 +1,25 @@
 #pragma once
 
 #include "GBFPawnComponentReadyInterface.h"
-#include "ModularPawnComponent.h"
 
+#include <Components/GameFrameworkInitStateInterface.h>
 #include <CoreMinimal.h>
+#include <ModularPawnComponent.h>
 
 #include "GBFPawnComponent.generated.h"
 
 UCLASS( Blueprintable, Meta = ( BlueprintSpawnableComponent ) )
-class GAMEBASEFRAMEWORK_API UGBFPawnComponent : public UModularPawnComponent, public IGBFPawnComponentReadyInterface
+class GAMEBASEFRAMEWORK_API UGBFPawnComponent : public UModularPawnComponent, public IGBFPawnComponentReadyInterface, public IGameFrameworkInitStateInterface
 {
     GENERATED_BODY()
 
 public:
-
     bool IsPawnComponentReadyToInitialize() const override;
+
+    void CheckDefaultInitialization() override;
+
+protected:
+    void OnRegister() override;
+    void BeginPlay() override;
+    void EndPlay( const EEndPlayReason::Type end_play_reason ) override;
 };
