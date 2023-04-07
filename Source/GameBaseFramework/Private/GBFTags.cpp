1a52183adeb1f3492be6e428f0a6abe8e51783f9
Michael Delva <michael.delva@fishingcactus.com>
2023-04-07 16:22:00 +0200

    Added InitState tags

---
+++ Source/GameBaseFramework/Private/GBFTags.cpp
@@ -0,0 +1,6 @@
+#include "GBFTags.h"
+
+UE_DEFINE_GAMEPLAY_TAG( GBF_InitState_Spawned, TEXT( "InitState.Spawned" ) );
+UE_DEFINE_GAMEPLAY_TAG( GBF_InitState_DataAvailable, TEXT( "InitState.DataAvailable" ) );
+UE_DEFINE_GAMEPLAY_TAG( GBF_InitState_DataInitialized, TEXT( "InitState.DataInitialized" ) );
+UE_DEFINE_GAMEPLAY_TAG( GBF_InitState_GameplayReady, TEXT( "InitState.GameplayReady" ) );
