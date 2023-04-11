67d21aabd80bb9982eab548e49287e04c04ec99f
Viktor Blomme <viktor.blomme@hotmail.com>
2023-04-11 20:19:12 +0200

    Create classes
    Create camera mode class.
    Create camera component.

---
+++ Source/GameBaseFramework/Private/Camera/GBFCameraComponent.cpp
@@ -0,0 +1,127 @@
+#include "Camera/GBFCameraComponent.h"
+
+#include "Camera/GBFCameraMode.h"
+
+#include <Engine/Canvas.h>
+#include <GameFramework/PlayerController.h>
+
+UGBFCameraComponent::UGBFCameraComponent( const FObjectInitializer & object_initializer ) :
+    Super( object_initializer )
+{
+    CameraModeStack = nullptr;
+    FieldOfViewOffset = 0.0f;
+}
+
+UGBFCameraComponent * UGBFCameraComponent::FindCameraComponent( const AActor * actor )
+{
+    return actor ? actor->FindComponentByClass< UGBFCameraComponent >() : nullptr;
+}
+
+void UGBFCameraComponent::DrawDebug( UCanvas * canvas ) const
+{
+    check( canvas );
+
+    auto & display_debug_manager = canvas->DisplayDebugManager;
+
+    display_debug_manager.SetFont( GEngine->GetSmallFont() );
+    display_debug_manager.SetDrawColor( FColor::Yellow );
+    display_debug_manager.DrawString( FString::Printf( TEXT( "GBFCameraComponent: %s" ), *GetNameSafe( GetTargetActor() ) ) );
+
+    display_debug_manager.SetDrawColor( FColor::White );
+    display_debug_manager.DrawString( FString::Printf( TEXT( "   Location: %s" ), *GetComponentLocation().ToCompactString() ) );
+    display_debug_manager.DrawString( FString::Printf( TEXT( "   Rotation: %s" ), *GetComponentRotation().ToCompactString() ) );
+    display_debug_manager.DrawString( FString::Printf( TEXT( "   FOV: %f" ), FieldOfView ) );
+
+    check( CameraModeStack );
+    CameraModeStack->DrawDebug( canvas );
+}
+
+void UGBFCameraComponent::AddFieldOfViewOffset( const float fov_offset )
+{
+    FieldOfViewOffset += fov_offset;
+}
+
+void UGBFCameraComponent::GetBlendInfo( float & out_weight_of_top_layer, FGameplayTag & out_tag_of_top_layer ) const
+{
+    check( CameraModeStack );
+    CameraModeStack->GetBlendInfo( out_weight_of_top_layer, out_tag_of_top_layer );
+}
+
+void UGBFCameraComponent::OnRegister()
+{
+    Super::OnRegister();
+
+    if ( !CameraModeStack )
+    {
+        CameraModeStack = NewObject< UGBFCameraModeStack >( this );
+        check( CameraModeStack );
+    }
+}
+
+void UGBFCameraComponent::GetCameraView( const float delta_time, FMinimalViewInfo & desired_view )
+{
+    check( CameraModeStack );
+
+    UpdateCameraModes();
+
+    FGBFCameraModeView camera_mode_view;
+    CameraModeStack->EvaluateStack( delta_time, camera_mode_view );
+
+    // Keep player controller in sync with the latest view.
+    if ( const auto * target_pawn = Cast< APawn >( GetTargetActor() ) )
+    {
+        if ( auto * pc = target_pawn->GetController< APlayerController >() )
+        {
+            pc->SetControlRotation( camera_mode_view.ControlRotation );
+        }
+    }
+
+    // Apply any offset that was added to the field of view.
+    camera_mode_view.FieldOfView += FieldOfViewOffset;
+    FieldOfViewOffset = 0.0f;
+
+    // Keep camera component in sync with the latest view.
+    SetWorldLocationAndRotation( camera_mode_view.Location, camera_mode_view.Rotation );
+    FieldOfView = camera_mode_view.FieldOfView;
+
+    // Fill in desired view.
+    desired_view.Location = camera_mode_view.Location;
+    desired_view.Rotation = camera_mode_view.Rotation;
+    desired_view.FOV = camera_mode_view.FieldOfView;
+    desired_view.OrthoWidth = OrthoWidth;
+    desired_view.OrthoNearClipPlane = OrthoNearClipPlane;
+    desired_view.OrthoFarClipPlane = OrthoFarClipPlane;
+    desired_view.AspectRatio = AspectRatio;
+    desired_view.bConstrainAspectRatio = bConstrainAspectRatio;
+    desired_view.bUseFieldOfViewForLOD = bUseFieldOfViewForLOD;
+    desired_view.ProjectionMode = ProjectionMode;
+
+    // See if the CameraActor wants to override the PostProcess settings used.
+    desired_view.PostProcessBlendWeight = PostProcessBlendWeight;
+    if ( PostProcessBlendWeight > 0.0f )
+    {
+        desired_view.PostProcessSettings = PostProcessSettings;
+    }
+
+    if ( IsXRHeadTrackedCamera() )
+    {
+        // In XR much of the camera behavior above is irrellevant, but the post process settings are not.
+        Super::GetCameraView( delta_time, desired_view );
+    }
+}
+
+void UGBFCameraComponent::UpdateCameraModes()
+{
+    check( CameraModeStack );
+
+    if ( CameraModeStack->IsStackActivate() )
+    {
+        if ( DetermineCameraModeDelegate.IsBound() )
+        {
+            if ( const auto camera_mode = DetermineCameraModeDelegate.Execute() )
+            {
+                CameraModeStack->PushCameraMode( camera_mode );
+            }
+        }
+    }
+}
---
+++ Source/GameBaseFramework/Public/Camera/GBFCameraMode.h
@@ -0,0 +1,203 @@
+#pragma once
+
+#include <CoreMinimal.h>
+#include <GameplayTagContainer.h>
+#include <UObject/Object.h>
+
+#include "GBFCameraMode.generated.h"
+
+class UGBFCameraComponent;
+class UCanvas;
+
+#define GBF_CAMERA_DEFAULT_FOV ( 80.0f )
+#define GBF_CAMERA_DEFAULT_PITCH_MIN ( -89.0f )
+#define GBF_CAMERA_DEFAULT_PITCH_MAX ( 89.0f )
+
+/**
+ * EGBFCameraModeBlendFunction
+ *
+ *	Blend function used for transitioning between camera modes.
+ */
+UENUM( BlueprintType )
+enum class EGBFCameraModeBlendFunction : uint8
+{
+    // Does a simple linear interpolation.
+    Linear,
+
+    // Immediately accelerates, but smoothly decelerates into the target.  Ease amount controlled by the exponent.
+    EaseIn,
+
+    // Smoothly accelerates, but does not decelerate into the target.  Ease amount controlled by the exponent.
+    EaseOut,
+
+    // Smoothly accelerates and decelerates.  Ease amount controlled by the exponent.
+    EaseInOut,
+
+    COUNT UMETA( Hidden )
+};
+
+/**
+ * FGBFCameraModeView
+ *
+ *	View data produced by the camera mode that is used to blend camera modes.
+ */
+struct FGBFCameraModeView
+{
+    FGBFCameraModeView();
+
+    void Blend( const FGBFCameraModeView & other, float other_weight );
+
+    FVector Location;
+    FRotator Rotation;
+    FRotator ControlRotation;
+    float FieldOfView;
+};
+
+/**
+ * UGBFCameraMode
+ *
+ *	Base class for all camera modes.
+ */
+UCLASS( Abstract, NotBlueprintable )
+class GAMEBASEFRAMEWORK_API UGBFCameraMode : public UObject
+{
+    GENERATED_BODY()
+
+public:
+    UGBFCameraMode();
+
+    UGBFCameraComponent * GetGBFCameraComponent() const;
+
+    const FGBFCameraModeView & GetCameraModeView() const;
+    float GetBlendTime() const;
+    float GetBlendWeight() const;
+    FGameplayTag GetCameraTypeTag() const;
+
+    UWorld * GetWorld() const override;
+
+    // Called when this camera mode is activated on the camera mode stack.
+    virtual void OnActivation() {};
+
+    // Called when this camera mode is deactivated on the camera mode stack.
+    virtual void OnDeactivation() {};
+
+    virtual void DrawDebug( UCanvas * canvas ) const;
+
+    AActor * GetTargetActor() const;
+    void UpdateCameraMode( float delta_time );
+    void SetBlendWeight( float weight );
+
+protected:
+    virtual FVector GetPivotLocation() const;
+    virtual FRotator GetPivotRotation() const;
+
+    virtual void UpdateView( float delta_time );
+    virtual void UpdateBlending( float delta_time );
+
+    // A tag that can be queried by gameplay code that cares when a kind of camera mode is active
+    // without having to ask about a specific mode (e.g., when aiming downsights to get more accuracy)
+    UPROPERTY( EditDefaultsOnly, Category = "Blending" )
+    FGameplayTag CameraTypeTag;
+
+    // The horizontal field of view (in degrees).
+    UPROPERTY( EditDefaultsOnly, Category = "View", Meta = ( UIMin = "5.0", UIMax = "170", ClampMin = "5.0", ClampMax = "170.0" ) )
+    float FieldOfView;
+
+    // Minimum view pitch (in degrees).
+    UPROPERTY( EditDefaultsOnly, Category = "View", Meta = ( UIMin = "-89.9", UIMax = "89.9", ClampMin = "-89.9", ClampMax = "89.9" ) )
+    float ViewPitchMin;
+
+    // Maximum view pitch (in degrees).
+    UPROPERTY( EditDefaultsOnly, Category = "View", Meta = ( UIMin = "-89.9", UIMax = "89.9", ClampMin = "-89.9", ClampMax = "89.9" ) )
+    float ViewPitchMax;
+
+    // How long it takes to blend in this mode.
+    UPROPERTY( EditDefaultsOnly, Category = "Blending" )
+    float BlendTime;
+
+    // Function used for blending.
+    UPROPERTY( EditDefaultsOnly, Category = "Blending" )
+    EGBFCameraModeBlendFunction BlendFunction;
+
+    // Exponent used by blend functions to control the shape of the curve.
+    UPROPERTY( EditDefaultsOnly, Category = "Blending" )
+    float BlendExponent;
+
+    /** If true, skips all interpolation and puts camera in ideal location.  Automatically set to false next frame. */
+    UPROPERTY( transient )
+    uint32 bResetInterpolation : 1;
+
+    // View output produced by the camera mode.
+    FGBFCameraModeView View;
+
+    // Linear blend alpha used to determine the blend weight.
+    float BlendAlpha;
+
+    // Blend weight calculated using the blend alpha and function.
+    float BlendWeight;
+};
+
+FORCEINLINE const FGBFCameraModeView & UGBFCameraMode::GetCameraModeView() const
+{
+    return View;
+}
+
+FORCEINLINE float UGBFCameraMode::GetBlendTime() const
+{
+    return BlendTime;
+}
+
+FORCEINLINE float UGBFCameraMode::GetBlendWeight() const
+{
+    return BlendWeight;
+}
+
+FORCEINLINE FGameplayTag UGBFCameraMode::GetCameraTypeTag() const
+{
+    return CameraTypeTag;
+}
+
+/**
+ * UGBFCameraModeStack
+ *
+ *	Stack used for blending camera modes.
+ */
+UCLASS()
+class UGBFCameraModeStack : public UObject
+{
+    GENERATED_BODY()
+
+public:
+    UGBFCameraModeStack();
+
+    bool IsStackActivate() const;
+
+    void ActivateStack();
+    void DeactivateStack();
+
+    void PushCameraMode( TSubclassOf< UGBFCameraMode > camera_mode_class );
+    bool EvaluateStack( float delta_time, FGBFCameraModeView & out_camera_mode_view );
+    void DrawDebug( UCanvas * canvas ) const;
+
+    // Gets the tag associated with the top layer and the blend weight of it
+    void GetBlendInfo( float & out_weight_of_top_layer, FGameplayTag & out_tag_of_top_layer ) const;
+
+protected:
+    UGBFCameraMode * GetCameraModeInstance( TSubclassOf< UGBFCameraMode > camera_mode_class );
+
+    void UpdateStack( float delta_time );
+    void BlendStack( FGBFCameraModeView & out_camera_mode_view ) const;
+
+    UPROPERTY()
+    TArray< TObjectPtr< UGBFCameraMode > > CameraModeInstances;
+
+    UPROPERTY()
+    TArray< TObjectPtr< UGBFCameraMode > > CameraModeStack;
+
+    bool bIsActive;
+};
+
+FORCEINLINE bool UGBFCameraModeStack::IsStackActivate() const
+{
+    return bIsActive;
+}
