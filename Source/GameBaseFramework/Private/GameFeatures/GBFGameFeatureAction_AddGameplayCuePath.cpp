922f43898e90ecda781fdb4e10c5bdf6a9cdf5e0
Michael Delva <michael@emidee.net>
2023-04-12 21:46:07 +0200

    Imported game features from GASExt

---
+++ Source/GameBaseFramework/Private/GameFeatures/GBFGameFeatureAction_AddGameplayCuePath.cpp
@@ -0,0 +1,104 @@
+#include "GameFeatures/GBFGameFeatureAction_AddGameplayCuePath.h"
+
+#include "GameplayCues/GASExtGameplayCueManager.h"
+
+#include <AbilitySystemGlobals.h>
+#include <GameFeatureData.h>
+#include <GameplayCueSet.h>
+
+#define LOCTEXT_NAMESPACE "GameFeatures"
+
+#if WITH_EDITOR
+EDataValidationResult UGBFGameFeatureAction_AddGameplayCuePath::IsDataValid( TArray< FText > & validation_errors )
+{
+    auto result = Super::IsDataValid( validation_errors );
+
+    auto error_reason = FText::GetEmpty();
+    for ( const FDirectoryPath & Directory : DirectoryPathsToAdd )
+    {
+        if ( Directory.Path.IsEmpty() )
+        {
+            const auto invalid_cue_path_error = FText::Format( LOCTEXT( "invalid_cue_path_error", "'{0}' is not a valid path!" ), FText::FromString( Directory.Path ) );
+            validation_errors.Emplace( invalid_cue_path_error );
+            validation_errors.Emplace( error_reason );
+            result = CombineDataValidationResults( result, EDataValidationResult::Invalid );
+        }
+    }
+
+    return CombineDataValidationResults( result, EDataValidationResult::Valid );
+}
+#endif
+
+void UGBFGameFeatureObserver_AddGameplayCuePath::OnGameFeatureRegistering( const UGameFeatureData * game_feature_data, const FString & plugin_name, const FString & /*plugin_url*/ )
+{
+    TRACE_CPUPROFILER_EVENT_SCOPE( ULyraGameFeature_AddGameplayCuePaths::OnGameFeatureRegistering );
+
+    const auto plugin_root_path = TEXT( "/" ) + plugin_name;
+    for ( const auto * action : game_feature_data->GetActions() )
+    {
+        if ( const auto * game_feature_action_add_gameplay_cue_path = Cast< UGBFGameFeatureAction_AddGameplayCuePath >( action ) )
+        {
+            const auto & dirs_to_add = game_feature_action_add_gameplay_cue_path->GetDirectoryPathsToAdd();
+
+            if ( auto * gameplay_cue_manager = UGASExtGameplayCueManager::Get() )
+            {
+                const auto * runtime_gameplay_cue_set = gameplay_cue_manager->GetRuntimeCueSet();
+                const auto pre_initialize_num_cues = runtime_gameplay_cue_set ? runtime_gameplay_cue_set->GameplayCueData.Num() : 0;
+
+                for ( const auto & [ path ] : dirs_to_add )
+                {
+                    auto mutable_path = path;
+                    UGameFeaturesSubsystem::FixPluginPackagePath( mutable_path, plugin_root_path, false );
+                    gameplay_cue_manager->AddGameplayCueNotifyPath( mutable_path, /** bShouldRescanCueAssets = */ false );
+                }
+
+                // Rebuild the runtime library with these new paths
+                if ( !dirs_to_add.IsEmpty() )
+                {
+                    gameplay_cue_manager->InitializeRuntimeObjectLibrary();
+                }
+
+                if ( const auto post_initialize_num_cues = runtime_gameplay_cue_set
+                                                               ? runtime_gameplay_cue_set->GameplayCueData.Num()
+                                                               : 0;
+                     pre_initialize_num_cues != post_initialize_num_cues )
+                {
+                    gameplay_cue_manager->RefreshGameplayCuePrimaryAsset();
+                }
+            }
+        }
+    }
+}
+
+void UGBFGameFeatureObserver_AddGameplayCuePath::OnGameFeatureUnregistering( const UGameFeatureData * game_feature_data, const FString & plugin_name, const FString & /*plugin_url*/ )
+{
+    const auto plugin_root_path = TEXT( "/" ) + plugin_name;
+    for ( const UGameFeatureAction * action : game_feature_data->GetActions() )
+    {
+        if ( const auto * game_feature_action_add_gameplay_cue_path = Cast< UGBFGameFeatureAction_AddGameplayCuePath >( game_feature_data ) )
+        {
+            const auto & dirs_to_add = game_feature_action_add_gameplay_cue_path->GetDirectoryPathsToAdd();
+
+            if ( auto * gcm = UAbilitySystemGlobals::Get().GetGameplayCueManager() )
+            {
+                int32 num_removed = 0;
+                for ( const auto & [ path ] : dirs_to_add )
+                {
+                    auto mutable_path = path;
+                    UGameFeaturesSubsystem::FixPluginPackagePath( mutable_path, plugin_root_path, false );
+                    num_removed += gcm->RemoveGameplayCueNotifyPath( mutable_path, /** bShouldRescanCueAssets = */ false );
+                }
+
+                ensure( num_removed == dirs_to_add.Num() );
+
+                // Rebuild the runtime library only if there is a need to
+                if ( num_removed > 0 )
+                {
+                    gcm->InitializeRuntimeObjectLibrary();
+                }
+            }
+        }
+    }
+}
+
+#undef LOCTEXT_NAMESPACE
