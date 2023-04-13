a51b1f6651dbfbf859a3b142a8317c07c375ca10
Michael Delva <michael@emidee.net>
2023-04-13 16:43:25 +0200

    coding convention for templates

--- Source/GameBaseFramework/Public/Input/GBFInputComponent.h
+++ Source/GameBaseFramework/Public/Input/GBFInputComponent.h
@@ -20,17 +20,17 @@ public:
     void AddInputMappings( const UGBFInputConfig * input_config, UEnhancedInputLocalPlayerSubsystem * input_system ) const;
     void RemoveInputMappings( const UGBFInputConfig * input_config, UEnhancedInputLocalPlayerSubsystem * input_system ) const;
 
-    template < class UserClass, typename FuncType >
-    void BindNativeAction( const UGBFInputConfig * input_config, const FGameplayTag & input_tag, ETriggerEvent trigger_event, UserClass * object, FuncType func, bool log_if_not_found );
+    template < class _USER_CLASS_, typename _FUNC_TYPE_ >
+    void BindNativeAction( const UGBFInputConfig * input_config, const FGameplayTag & input_tag, ETriggerEvent trigger_event, _USER_CLASS_ * object, _FUNC_TYPE_ func, bool log_if_not_found );
 
-    template < class UserClass, typename PressedFuncType, typename ReleasedFuncType >
-    void BindAbilityActions( const UGBFInputConfig * input_config, UserClass * object, PressedFuncType pressed_func, ReleasedFuncType released_func, TArray< uint32 > & bind_handles );
+    template < class _USER_CLASS_, typename _PRESSED_FUNC_, typename _RELEASED_FUNC_ >
+    void BindAbilityActions( const UGBFInputConfig * input_config, _USER_CLASS_ * object, _PRESSED_FUNC_ pressed_func, _RELEASED_FUNC_ released_func, TArray< uint32 > & bind_handles );
 
     void RemoveBinds( TArray< uint32 > & bind_handles );
 };
 
-template < class UserClass, typename FuncType >
-void UGBFInputComponent::BindNativeAction( const UGBFInputConfig * input_config, const FGameplayTag & input_tag, ETriggerEvent trigger_event, UserClass * object, FuncType func, bool log_if_not_found )
+template < class _USER_CLASS_, typename _FUNC_TYPE_ >
+void UGBFInputComponent::BindNativeAction( const UGBFInputConfig * input_config, const FGameplayTag & input_tag, ETriggerEvent trigger_event, _USER_CLASS_ * object, _FUNC_TYPE_ func, bool log_if_not_found )
 {
     check( input_config != nullptr );
     if ( const auto * input_action = input_config->FindNativeInputActionForTag( input_tag, log_if_not_found ) )
@@ -39,10 +39,10 @@ void UGBFInputComponent::BindNativeAction( const UGBFInputConfig * input_config,
     }
 }
 
-template < class UserClass, typename PressedFuncType, typename ReleasedFuncType >
-void UGBFInputComponent::BindAbilityActions( const UGBFInputConfig * input_config, UserClass * object, PressedFuncType pressed_func, ReleasedFuncType released_func, TArray< uint32 > & bind_handles )
+template < class _USER_CLASS_, typename _PRESSED_FUNC_, typename _RELEASED_FUNC_ >
+void UGBFInputComponent::BindAbilityActions( const UGBFInputConfig * input_config, _USER_CLASS_ * object, _PRESSED_FUNC_ pressed_func, _RELEASED_FUNC_ released_func, TArray< uint32 > & bind_handles )
 {
-    check( input_config );
+    check( input_config != nullptr );
 
     for ( const auto & [ input_action, input_tag ] : input_config->AbilityInputActions )
     {
