224ee9929a586fb3308e9805255c17fe0323afdd
Michael Delva <michael@emidee.net>
2023-04-17 11:56:00 +0200

    Updated experience definition with conditional actions

--- Source/GameBaseFramework/Private/GameFramework/Experiences/GBFExperienceDefinition.cpp
+++ Source/GameBaseFramework/Private/GameFramework/Experiences/GBFExperienceDefinition.cpp
@@ -1,11 +1,22 @@
 #include "GameFramework/Experiences/GBFExperienceDefinition.h"
 
+#include "DVEDataValidator.h"
 #include "GameFramework/Experiences/GBFExperienceActionSet.h"
 
-#include <Engine/World.h>
 #include <GameFeatureAction.h>
 
 #define LOCTEXT_NAMESPACE "GameBaseFrameworkSystem"
+
+#if WITH_EDITOR
+EDataValidationResult FGBFExperienceDefinitionActions::IsDataValid( TArray< FText > & validation_errors ) const
+{
+    return FDVEDataValidator( validation_errors )
+        .NoEmptyItem( VALIDATOR_GET_PROPERTY( GameFeaturesToEnable ) )
+        .NoNullItem( VALIDATOR_GET_PROPERTY( Actions ) )
+        .NoNullItem( VALIDATOR_GET_PROPERTY( ActionSets ) )
+        .Result();
+}
+#endif
 
 FPrimaryAssetId UGBFExperienceDefinition::GetPrimaryAssetId() const
 {
@@ -18,26 +29,67 @@ FPrimaryAssetType UGBFExperienceDefinition::GetPrimaryAssetType()
     return PrimaryAssetType;
 }
 
+const UGBFExperienceDefinition * UGBFExperienceDefinition::Resolve( UWorld * world ) const
+{
+    if ( ConditionalActions.IsEmpty() )
+    {
+        return this;
+    }
+
+    auto * new_experience = NewObject< UGBFExperienceDefinition >();
+    new_experience->DefaultActions = DefaultActions;
+
+    for ( const auto & conditional_action : ConditionalActions )
+    {
+        if ( !ensureAlways( conditional_action.Condition != nullptr ) )
+        {
+            continue;
+        }
+
+        if ( conditional_action.Condition->CanApplyActions( world ) )
+        {
+            new_experience->ActionSets.Append( conditional_action.Actions.ActionSets );
+            new_experience->Actions.Append( conditional_action.Actions.Actions );
+            new_experience->GameFeaturesToEnable.Append( conditional_action.Actions.GameFeaturesToEnable );
+        }
+    }
+
+    return new_experience;
+}
+
+void UGBFExperienceDefinition::Serialize( FArchive & ar )
+{
+    if ( ar.IsSaving() )
+    {
+        const auto add_if_not_empty = [ & ]( auto & array, const auto & other_array ) {
+            if ( array.IsEmpty() )
+            {
+                array.Append( other_array );
+            }
+        };
+
+        add_if_not_empty( DefaultActions.ActionSets, ActionSets );
+        add_if_not_empty( DefaultActions.Actions, Actions );
+        add_if_not_empty( DefaultActions.GameFeaturesToEnable, GameFeaturesToEnable );
+    }
+
+    Super::Serialize( ar );
+}
+
 #if WITH_EDITOR
 EDataValidationResult UGBFExperienceDefinition::IsDataValid( TArray< FText > & validation_errors )
 {
-    auto result = CombineDataValidationResults( Super::IsDataValid( validation_errors ), EDataValidationResult::Valid );
+    auto result = CombineDataValidationResults( Super::IsDataValid( validation_errors ), DefaultActions.IsDataValid( validation_errors ) );
 
-    auto entry_index = 0;
-    for ( auto * action : Actions )
+    for ( const auto & conditional_action : ConditionalActions )
     {
-        if ( action != nullptr )
+        if ( conditional_action.Condition == nullptr )
         {
-            const auto child_result = action->IsDataValid( validation_errors );
-            result = CombineDataValidationResults( result, child_result );
-        }
-        else
-        {
-            result = EDataValidationResult::Invalid;
-            validation_errors.Add( FText::Format( LOCTEXT( "ActionEntryIsNull", "Null entry at index {0} in Actions" ), FText::AsNumber( entry_index ) ) );
-        }
+            validation_errors.Add( FText::FromString( TEXT( "You can't have a null conditional action." ) ) );
 
-        ++entry_index;
+            result = EDataValidationResult::Invalid;
+        }
+        result = CombineDataValidationResults( result, conditional_action.Actions.IsDataValid( validation_errors ) );
     }
 
     // Make sure users didn't subclass from a BP of this (it's fine and expected to subclass once in BP, just not twice)
@@ -70,12 +122,21 @@ void UGBFExperienceDefinition::UpdateAssetBundleData()
 {
     Super::UpdateAssetBundleData();
 
-    for ( auto * action : Actions )
-    {
-        if ( action != nullptr )
+    const auto update_action_bundle_data = [ & ]( const TArray< UGameFeatureAction * > & actions ) {
+        for ( auto * action : actions )
         {
-            action->AddAdditionalAssetBundleData( AssetBundleData );
+            if ( action != nullptr )
+            {
+                action->AddAdditionalAssetBundleData( AssetBundleData );
+            }
         }
+    };
+
+    update_action_bundle_data( DefaultActions.Actions );
+
+    for ( const auto & conditional_action : ConditionalActions )
+    {
+        update_action_bundle_data( conditional_action.Actions.Actions );
     }
 }
 #endif
--- Source/GameBaseFramework/Public/GameFramework/Experiences/GBFExperienceDefinition.h
+++ Source/GameBaseFramework/Public/GameFramework/Experiences/GBFExperienceDefinition.h
@@ -10,6 +10,50 @@ class UGBFPawnData;
 class UGBFExperienceActionSet;
 class UGameFeatureAction;
 
+USTRUCT()
+struct GAMEBASEFRAMEWORK_API FGBFExperienceDefinitionActions
+{
+    GENERATED_USTRUCT_BODY()
+
+#if WITH_EDITOR
+    EDataValidationResult IsDataValid( TArray< FText > & validation_errors ) const;
+#endif
+
+    // List of Game Feature Plugins this experience wants to have active
+    UPROPERTY( EditDefaultsOnly, Category = Gameplay )
+    TArray< FString > GameFeaturesToEnable;
+
+    // List of actions to perform as this experience is loaded/activated/deactivated/unloaded
+    UPROPERTY( EditDefaultsOnly, Instanced, Category = "Actions" )
+    TArray< UGameFeatureAction * > Actions;
+
+    // List of additional action sets to compose into this experience
+    UPROPERTY( EditDefaultsOnly, Category = Gameplay )
+    TArray< UGBFExperienceActionSet * > ActionSets;
+};
+
+UCLASS( DefaultToInstanced, Blueprintable, Abstract )
+class GAMEBASEFRAMEWORK_API UGBFExperienceCondition : public UObject
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION( BlueprintImplementableEvent )
+    bool CanApplyActions( UWorld * world ) const;
+};
+
+USTRUCT()
+struct GAMEBASEFRAMEWORK_API FGBFExperienceConditionalActions
+{
+    GENERATED_USTRUCT_BODY()
+
+    UPROPERTY( EditDefaultsOnly, Instanced )
+    TObjectPtr< UGBFExperienceCondition > Condition;
+
+    UPROPERTY()
+    FGBFExperienceDefinitionActions Actions;
+};
+
 UCLASS( BlueprintType, Const )
 class GAMEBASEFRAMEWORK_API UGBFExperienceDefinition final : public UPrimaryDataAsset
 {
@@ -20,6 +64,10 @@ public:
 
     static FPrimaryAssetType GetPrimaryAssetType();
 
+    const UGBFExperienceDefinition * Resolve( UWorld * world ) const;
+
+    void Serialize( FArchive & ar ) override;
+
 #if WITH_EDITOR
     EDataValidationResult IsDataValid( TArray< FText > & validation_errors ) override;
 #endif
@@ -27,6 +75,12 @@ public:
 #if WITH_EDITORONLY_DATA
     void UpdateAssetBundleData() override;
 #endif
+
+    UPROPERTY( EditDefaultsOnly )
+    FGBFExperienceDefinitionActions DefaultActions;
+
+    UPROPERTY( EditDefaultsOnly )
+    TArray< FGBFExperienceConditionalActions > ConditionalActions;
 
     // List of Game Feature Plugins this experience wants to have active
     UPROPERTY( EditDefaultsOnly, Category = Gameplay )
