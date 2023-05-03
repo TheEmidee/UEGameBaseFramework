47c503d56b0080288e795ae977b2d3229a7f90b2
Viktor Blomme <viktor.blomme@hotmail.com>
2023-05-03 15:40:07 +0200

    Inherit from GASExt hitscan AT
    Setup looping the AT.

--- Source/GameBaseFramework/Private/Interaction/Tasks/GBFAT_WaitForInteractableTargets_SingleLineTrace.cpp
+++ Source/GameBaseFramework/Private/Interaction/Tasks/GBFAT_WaitForInteractableTargets_SingleLineTrace.cpp
@@ -7,100 +7,24 @@
 #include <Engine/World.h>
 #include <TimerManager.h>
 
-UGBFAT_WaitForInteractableTargets_SingleLineTrace::UGBFAT_WaitForInteractableTargets_SingleLineTrace( const FObjectInitializer & object_initializer ) :
-    Super( object_initializer )
+UGBFAT_WaitForInteractableTargets_SingleLineTrace::UGBFAT_WaitForInteractableTargets_SingleLineTrace()
 {
+    bEndTaskWhenTargetDataSent = false;
+    TargetDataProductionRate = 1.0f;
 }
 
 UGBFAT_WaitForInteractableTargets_SingleLineTrace * UGBFAT_WaitForInteractableTargets_SingleLineTrace::WaitForInteractableTargets_SingleLineTrace(
     UGameplayAbility * owning_ability,
-    const FGBFInteractionQuery interaction_query,
-    const FCollisionProfileName trace_profile,
-    const FGameplayAbilityTargetingLocationInfo start_location,
-    const float interaction_scan_range,
-    const float interaction_scan_rate,
-    bool aim_with_player_controller /*= true*/,
-    const bool show_debug /*= false*/ )
+    const FName task_instance_name,
+    const FGameplayAbilityTargetingLocationInfo & start_trace_location_infos,
+    const FGASExtWaitTargetDataReplicationOptions & replication_options,
+    const FGASExtWaitTargetDataHitScanOptions & hit_scan_options,
+    const float interaction_scan_rate /*= 1.0f*/ )
 {
-    auto * my_obj = NewAbilityTask< UGBFAT_WaitForInteractableTargets_SingleLineTrace >( owning_ability );
-    my_obj->InteractionScanRange = interaction_scan_range;
-    my_obj->InteractionScanRate = interaction_scan_rate;
-    my_obj->StartLocation = start_location;
-    my_obj->InteractionQuery = interaction_query;
-    my_obj->TraceProfile = trace_profile;
-    my_obj->bAimWithPlayerController = aim_with_player_controller;
-    my_obj->bShowDebug = show_debug;
-
+    auto * my_obj = NewAbilityTask< UGBFAT_WaitForInteractableTargets_SingleLineTrace >( owning_ability, task_instance_name );
+    my_obj->StartLocationInfo = start_trace_location_infos;
+    my_obj->ReplicationOptions = replication_options;
+    my_obj->Options = hit_scan_options;
+    my_obj->TargetDataProductionRate = interaction_scan_rate;
     return my_obj;
-}
-
-void UGBFAT_WaitForInteractableTargets_SingleLineTrace::Activate()
-{
-    SetWaitingOnAvatar();
-
-    const auto * world = GetWorld();
-    world->GetTimerManager().SetTimer( TimerHandle, this, &ThisClass::PerformTrace, InteractionScanRate, true );
-}
-
-void UGBFAT_WaitForInteractableTargets_SingleLineTrace::OnDestroy( const bool ability_ended )
-{
-    if ( const auto * world = GetWorld() )
-    {
-        world->GetTimerManager().ClearTimer( TimerHandle );
-    }
-
-    Super::OnDestroy( ability_ended );
-}
-
-void UGBFAT_WaitForInteractableTargets_SingleLineTrace::PerformTrace()
-{
-    auto * avatar_actor = Ability->GetCurrentActorInfo()->AvatarActor.Get();
-    if ( avatar_actor == nullptr )
-    {
-        return;
-    }
-
-    auto * world = GetWorld();
-
-    TArray< AActor * > actors_to_ignore;
-    actors_to_ignore.Add( avatar_actor );
-
-    FCollisionQueryParams params( SCENE_QUERY_STAT( UAbilityTask_WaitForInteractableTargets_SingleLineTrace ), false );
-    params.AddIgnoredActors( actors_to_ignore );
-
-    auto trace_start = StartLocation.GetTargetingTransform().GetLocation();
-    FVector trace_end;
-    if ( bAimWithPlayerController )
-    {
-        AimWithPlayerController( avatar_actor, params, trace_start, InteractionScanRange, trace_end );
-    }
-    else
-    {
-        const auto forward_vector = avatar_actor->GetActorForwardVector();
-        trace_end = trace_start + forward_vector * InteractionScanRange;
-    }
-
-    FHitResult out_hit_result;
-    LineTrace( out_hit_result, world, trace_start, trace_end, TraceProfile.Name, params );
-
-    TArray< TScriptInterface< IGBFInteractableTarget > > interactable_targets;
-    UGBFInteractionStatics::AppendInteractableTargetsFromHitResult( out_hit_result, interactable_targets );
-
-    UpdateInteractableOptions( InteractionQuery, interactable_targets );
-
-#if ENABLE_DRAW_DEBUG
-    if ( bShowDebug )
-    {
-        auto debug_color = out_hit_result.bBlockingHit ? FColor::Red : FColor::Green;
-        if ( out_hit_result.bBlockingHit )
-        {
-            DrawDebugLine( world, trace_start, out_hit_result.Location, debug_color, false, InteractionScanRate );
-            DrawDebugSphere( world, out_hit_result.Location, 5, 16, debug_color, false, InteractionScanRate );
-        }
-        else
-        {
-            DrawDebugLine( world, trace_start, trace_end, debug_color, false, InteractionScanRate );
-        }
-    }
-#endif
 }
--- Source/GameBaseFramework/Public/Interaction/Tasks/GBFAT_WaitForInteractableTargets_SingleLineTrace.h
+++ Source/GameBaseFramework/Public/Interaction/Tasks/GBFAT_WaitForInteractableTargets_SingleLineTrace.h
@@ -2,46 +2,25 @@
 
 #include "Interaction/GBFInteractionQuery.h"
 #include "Interaction/Tasks/GBFAT_WaitForInteractableTargets.h"
+#include "Tasks/GASExtAT_WaitTargetDataHitScan.h"
 
 #include "GBFAT_WaitForInteractableTargets_SingleLineTrace.generated.h"
 
 UCLASS()
-class GAMEBASEFRAMEWORK_API UGBFAT_WaitForInteractableTargets_SingleLineTrace : public UGBFAT_WaitForInteractableTargets
+class GAMEBASEFRAMEWORK_API UGBFAT_WaitForInteractableTargets_SingleLineTrace : public UGASExtAT_WaitTargetDataHitScan
 {
     GENERATED_BODY()
 
-    UGBFAT_WaitForInteractableTargets_SingleLineTrace( const FObjectInitializer & object_initializer );
-
-    void Activate() override;
+public:
+    UGBFAT_WaitForInteractableTargets_SingleLineTrace();
 
     /** Wait until we trace new set of interactables.  This task automatically loops. */
     UFUNCTION( BlueprintCallable, Category = "Ability|Tasks", meta = ( HidePin = "owning_ability", DefaultToSelf = "owning_ability", BlueprintInternalUseOnly = true ) )
     static UGBFAT_WaitForInteractableTargets_SingleLineTrace * WaitForInteractableTargets_SingleLineTrace(
         UGameplayAbility * owning_ability,
-        FGBFInteractionQuery interaction_query,
-        FCollisionProfileName trace_profile,
-        FGameplayAbilityTargetingLocationInfo start_location,
-        float interaction_scan_range = 100,
-        float interaction_scan_rate = 0.100,
-        bool aim_with_player_controller = true,
-        bool show_debug = false );
-
-private:
-    void OnDestroy( bool ability_ended ) override;
-    void PerformTrace();
-
-    UPROPERTY()
-    FGBFInteractionQuery InteractionQuery;
-
-    UPROPERTY()
-    FGameplayAbilityTargetingLocationInfo StartLocation;
-
-    UPROPERTY()
-    uint8 bAimWithPlayerController : 1;
-
-    float InteractionScanRange = 100;
-    float InteractionScanRate = 0.100;
-    bool bShowDebug = false;
-
-    FTimerHandle TimerHandle;
+        FName task_instance_name,
+        const FGameplayAbilityTargetingLocationInfo & start_trace_location_infos,
+        const FGASExtWaitTargetDataReplicationOptions & replication_options,
+        const FGASExtWaitTargetDataHitScanOptions & hit_scan_options,
+        float interaction_scan_rate = 1.0f );
 };
