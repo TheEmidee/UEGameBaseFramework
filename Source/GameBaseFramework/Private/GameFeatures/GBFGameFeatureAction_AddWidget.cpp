5c973f50253972a914d7d8e126c98d273488f401
Michael Delva <michael.delva@fishingcactus.com>
2023-06-16 13:58:53 +0200

    Added GBFGameFeatureAction_AddWidget (missing UI Extension)

---
+++ Source/GameBaseFramework/Private/GameFeatures/GBFGameFeatureAction_AddWidget.cpp
@@ -0,0 +1,172 @@
+#include "GameFeatures/GBFGameFeatureAction_AddWidget.h"
+
+#include "Engine/GBFHUD.h"
+
+#include <CommonActivatableWidget.h>
+#include <CommonUIExtensions.h>
+#include <Components/GameFrameworkComponentManager.h>
+#include <Engine/LocalPlayer.h>
+#include <GameFeaturesSubsystemSettings.h>
+#include <GameFramework/PlayerController.h>
+
+#define LOCTEXT_NAMESPACE "GameBaseFrameworkGameFeatures"
+
+void UGBFGameFeatureAction_AddWidget::OnGameFeatureDeactivating( FGameFeatureDeactivatingContext & context )
+{
+    Super::OnGameFeatureDeactivating( context );
+
+    auto * active_data = ContextData.Find( context );
+    if ( ensure( active_data != nullptr ) )
+    {
+        Reset( *active_data );
+    }
+}
+
+#if WITH_EDITORONLY_DATA
+void UGBFGameFeatureAction_AddWidget::AddAdditionalAssetBundleData( FAssetBundleData & asset_bundle_data )
+{
+    for ( const auto & entry : Widgets )
+    {
+        asset_bundle_data.AddBundleAsset( UGameFeaturesSubsystemSettings::LoadStateClient, entry.WidgetClass.ToSoftObjectPath().GetAssetPath() );
+    }
+}
+#endif
+
+#if WITH_EDITOR
+EDataValidationResult UGBFGameFeatureAction_AddWidget::IsDataValid( TArray< FText > & validation_errors )
+{
+    auto result = CombineDataValidationResults( Super::IsDataValid( validation_errors ), EDataValidationResult::Valid );
+
+    {
+        auto entry_index = 0;
+        for ( const auto & [ layout_class, layer_id ] : Layout )
+        {
+            if ( layout_class.IsNull() )
+            {
+                result = EDataValidationResult::Invalid;
+                validation_errors.Add( FText::Format( LOCTEXT( "LayoutHasNullClass", "Null WidgetClass at index {0} in Layout" ), FText::AsNumber( entry_index ) ) );
+            }
+
+            if ( !layer_id.IsValid() )
+            {
+                result = EDataValidationResult::Invalid;
+                validation_errors.Add( FText::Format( LOCTEXT( "LayoutHasNoTag", "LayerID is not set at index {0} in Widgets" ), FText::AsNumber( entry_index ) ) );
+            }
+
+            ++entry_index;
+        }
+    }
+
+    {
+        auto entry_index = 0;
+        for ( const auto & [ widget_class, slot_id ] : Widgets )
+        {
+            if ( widget_class.IsNull() )
+            {
+                result = EDataValidationResult::Invalid;
+                validation_errors.Add( FText::Format( LOCTEXT( "EntryHasNullClass", "Null WidgetClass at index {0} in Widgets" ), FText::AsNumber( entry_index ) ) );
+            }
+
+            if ( !slot_id.IsValid() )
+            {
+                result = EDataValidationResult::Invalid;
+                validation_errors.Add( FText::Format( LOCTEXT( "EntryHasNoTag", "SlotID is not set at index {0} in Widgets" ), FText::AsNumber( entry_index ) ) );
+            }
+            ++entry_index;
+        }
+    }
+
+    return result;
+}
+
+#endif
+
+void UGBFGameFeatureAction_AddWidget::AddToWorld( const FWorldContext & world_context, const FGameFeatureStateChangeContext & change_context )
+{
+    const auto * world = world_context.World();
+    const auto game_instance = world_context.OwningGameInstance;
+    auto & active_data = ContextData.FindOrAdd( change_context );
+
+    if ( game_instance != nullptr && world != nullptr && world->IsGameWorld() )
+    {
+        if ( auto * component_manager = UGameInstance::GetSubsystem< UGameFrameworkComponentManager >( game_instance ) )
+        {
+            const TSoftClassPtr< AActor > hud_actor_class = AGBFHUD::StaticClass();
+
+            const auto extension_request_handle = component_manager->AddExtensionHandler(
+                hud_actor_class,
+                UGameFrameworkComponentManager::FExtensionHandlerDelegate::CreateUObject( this, &ThisClass::HandleActorExtension, change_context ) );
+
+            active_data.ComponentRequests.Add( extension_request_handle );
+        }
+    }
+}
+
+void UGBFGameFeatureAction_AddWidget::Reset( FPerContextData & active_data )
+{
+    active_data.ComponentRequests.Empty();
+    active_data.LayoutsAdded.Empty();
+
+    for ( FUIExtensionHandle & Handle : active_data.ExtensionHandles )
+    {
+        Handle.Unregister();
+    }
+    active_data.ExtensionHandles.Reset();
+}
+
+void UGBFGameFeatureAction_AddWidget::HandleActorExtension( AActor * actor, const FName event_name, const FGameFeatureStateChangeContext change_context )
+{
+    auto & active_data = ContextData.FindOrAdd( change_context );
+    if ( event_name == UGameFrameworkComponentManager::NAME_ExtensionRemoved || event_name == UGameFrameworkComponentManager::NAME_ReceiverRemoved )
+    {
+        RemoveWidgets( actor, active_data );
+    }
+    else if ( event_name == UGameFrameworkComponentManager::NAME_ExtensionAdded || event_name == UGameFrameworkComponentManager::NAME_GameActorReady )
+    {
+        AddWidgets( actor, active_data );
+    }
+}
+
+void UGBFGameFeatureAction_AddWidget::AddWidgets( AActor * actor, FPerContextData & active_data )
+{
+    const auto * hud = CastChecked< AGBFHUD >( actor );
+
+    if ( auto * local_player = Cast< ULocalPlayer >( hud->GetOwningPlayerController()->Player ) )
+    {
+        for ( const auto & entry : Layout )
+        {
+            if ( const auto concrete_widget_class = entry.LayoutClass.Get() )
+            {
+                active_data.LayoutsAdded.Add( UCommonUIExtensions::PushContentToLayer_ForPlayer( local_player, entry.LayerID, concrete_widget_class ) );
+            }
+        }
+
+        auto * extension_subsystem = hud->GetWorld()->GetSubsystem< UUIExtensionSubsystem >();
+        for ( const auto & entry : Widgets )
+        {
+            active_data.ExtensionHandles.Add( extension_subsystem->RegisterExtensionAsWidgetForContext( entry.SlotID, local_player, entry.WidgetClass.Get(), -1 ) );
+        }
+    }
+}
+
+void UGBFGameFeatureAction_AddWidget::RemoveWidgets( AActor * actor, FPerContextData & active_data )
+{
+    const auto * hud = CastChecked< AGBFHUD >( actor );
+
+    for ( auto & added_layout : active_data.LayoutsAdded )
+    {
+        if ( added_layout.IsValid() )
+        {
+            added_layout->DeactivateWidget();
+        }
+    }
+    active_data.LayoutsAdded.Reset();
+
+    for ( auto & handle : active_data.ExtensionHandles )
+    {
+        handle.Unregister();
+    }
+    active_data.ExtensionHandles.Reset();
+}
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/GameBaseFramework/Public/GameFeatures/GBFGameFeatureAction_AddWidget.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#include "GameFeatures/GBFGameFeatureAction_WorldActionBase.h"
+
+#include <CoreMinimal.h>
+
+#include "GBFGameFeatureAction_AddWidget.generated.h"
+
+class UCommonActivatableWidget;
+struct FWorldContext;
+struct FComponentRequestHandle;
+
+USTRUCT()
+struct FGBFHUDLayoutRequest
+{
+    GENERATED_BODY()
+
+    // The layout widget to spawn
+    UPROPERTY( EditAnywhere, Category = UI, meta = ( AssetBundles = "Client" ) )
+    TSoftClassPtr< UCommonActivatableWidget > LayoutClass;
+
+    // The layer to insert the widget in
+    UPROPERTY( EditAnywhere, Category = UI, meta = ( Categories = "UI.Layer" ) )
+    FGameplayTag LayerID;
+};
+
+USTRUCT()
+struct FGBFHUDElementEntry
+{
+    GENERATED_BODY()
+
+    // The widget to spawn
+    UPROPERTY( EditAnywhere, Category = UI, meta = ( AssetBundles = "Client" ) )
+    TSoftClassPtr< UUserWidget > WidgetClass;
+
+    // The slot ID where we should place this widget
+    UPROPERTY( EditAnywhere, Category = UI )
+    FGameplayTag SlotID;
+};
+
+/*
+ * Game feature to push widget content to layer of a local player, and to add extensions to an existing UI
+ */
+UCLASS( MinimalAPI, meta = ( DisplayName = "Add Widgets" ) )
+class UGBFGameFeatureAction_AddWidget final : public UGBFGameFeatureAction_WorldActionBase
+{
+    GENERATED_BODY()
+
+public:
+    void OnGameFeatureDeactivating( FGameFeatureDeactivatingContext & context ) override;
+
+#if WITH_EDITORONLY_DATA
+    void AddAdditionalAssetBundleData( FAssetBundleData & asset_bundle_data ) override;
+#endif
+
+#if WITH_EDITOR
+    EDataValidationResult IsDataValid( TArray< FText > & validation_errors ) override;
+#endif
+
+private:
+    struct FPerContextData
+    {
+        TArray< TSharedPtr< FComponentRequestHandle > > ComponentRequests;
+        TArray< TWeakObjectPtr< UCommonActivatableWidget > > LayoutsAdded;
+        TArray< FUIExtensionHandle > ExtensionHandles;
+    };
+
+    void AddToWorld( const FWorldContext & world_context, const FGameFeatureStateChangeContext & change_context ) override;
+    void Reset( FPerContextData & active_data );
+    void HandleActorExtension( AActor * actor, FName event_name, FGameFeatureStateChangeContext change_context );
+    void AddWidgets( AActor * actor, FPerContextData & active_data );
+    void RemoveWidgets( AActor * actor, FPerContextData & active_data );
+
+    // Layout to add to the HUD
+    UPROPERTY( EditAnywhere, Category = UI, meta = ( TitleProperty = "{LayerID} -> {LayoutClass}" ) )
+    TArray< FGBFHUDLayoutRequest > Layout;
+
+    // Widgets to add to the HUD
+    UPROPERTY( EditAnywhere, Category = UI, meta = ( TitleProperty = "{SlotID} -> {WidgetClass}" ) )
+    TArray< FGBFHUDElementEntry > Widgets;
+
+    TMap< FGameFeatureStateChangeContext, FPerContextData > ContextData;
+};
