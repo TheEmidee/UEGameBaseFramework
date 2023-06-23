0d03aa49d3b8ecabef5321b0db205baa28d2d176
Michael Delva <michael.delva@fishingcactus.com>
2023-06-23 10:13:56 +0200

    Fixed linking issues with UGBFGameFeatureAction_WorldActionBase

--- Source/GameBaseFramework/Public/GameFeatures/GBFGameFeatureAction_WorldActionBase.h
+++ Source/GameBaseFramework/Public/GameFeatures/GBFGameFeatureAction_WorldActionBase.h
@@ -11,14 +11,14 @@ class UGameInstance;
 struct FWorldContext;
 
 // This is a copy / paste from the one you can find in the ValleyOfTheAncient UE5 sample
-UCLASS( Abstract )
+UCLASS( Abstract, MinimalAPI )
 class UGBFGameFeatureAction_WorldActionBase : public UGameFeatureAction
 {
     GENERATED_BODY()
 
 public:
-    void OnGameFeatureActivating( FGameFeatureActivatingContext & context ) override;
-    void OnGameFeatureDeactivating( FGameFeatureDeactivatingContext & context ) override;
+    GAMEBASEFRAMEWORK_API void OnGameFeatureActivating( FGameFeatureActivatingContext & context ) override;
+    GAMEBASEFRAMEWORK_API void OnGameFeatureDeactivating( FGameFeatureDeactivatingContext & context ) override;
 
 private:
     void HandleGameInstanceStart( UGameInstance * game_instance, FGameFeatureStateChangeContext change_context );
