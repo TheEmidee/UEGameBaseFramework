5a7fae2b2c1e49ff2b239696367598079ada7fff
Viktor <viktor.blomme@fishingcactus.com>
2023-10-05 16:49:38 +0200

    Fix crash when assigning a layout class in game ui policy asset.

--- Source/CommonGame/Source/Private/GameUIPolicy.cpp
+++ Source/CommonGame/Source/Private/GameUIPolicy.cpp
@@ -30,12 +30,20 @@ UGameUIPolicy* UGameUIPolicy::GetGameUIPolicy(const UObject* WorldContextObject)
 
 UGameUIManagerSubsystem* UGameUIPolicy::GetOwningUIManager() const
 {
-	return CastChecked<UGameUIManagerSubsystem>(GetOuter());
+	return Cast<UGameUIManagerSubsystem>(GetOuter());
 }
 
 UWorld* UGameUIPolicy::GetWorld() const
 {
-	return GetOwningUIManager()->GetGameInstance()->GetWorld();
+    if ( const auto * owning_ui_manager = GetOwningUIManager())
+    {
+        if ( const auto * game_instance = owning_ui_manager->GetGameInstance())
+        {
+            return game_instance->GetWorld();
+        }
+    }
+    
+	return nullptr;
 }
 
 UPrimaryGameLayout* UGameUIPolicy::GetRootLayout(const UCommonLocalPlayer* LocalPlayer) const
