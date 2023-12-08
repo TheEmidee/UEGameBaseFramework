265105dd2fa7043fac5fc3bd0a5895e8488a48bb
Viktor Blomme <viktor.blomme@gmail.com>
2023-12-08 12:06:09 +0100

    Create interactable component

---
+++ Source/GameBaseFramework/Private/Interaction/GBFInteractableComponent.cpp
@@ -0,0 +1,12 @@
+﻿#include "Interaction/GBFInteractableComponent.h"
+
+UGBFInteractableComponent::UGBFInteractableComponent()
+{
+    PrimaryComponentTick.bStartWithTickEnabled = false;
+    PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UGBFInteractableComponent::GatherInteractionOptions( const FGBFInteractionQuery & interact_query, FGBFInteractionOptionBuilder & option_builder )
+{
+    option_builder.AddInteractionOption( Option );
+}
---
+++ Source/GameBaseFramework/Public/Interaction/GBFInteractableComponent.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "GBFInteractableTarget.h"
+
+#include <Components/ActorComponent.h>
+#include <CoreMinimal.h>
+
+#include "GBFInteractableComponent.generated.h"
+
+UCLASS( meta = ( BlueprintSpawnableComponent ) )
+class GAMEBASEFRAMEWORK_API UGBFInteractableComponent : public UActorComponent, public IGBFInteractableTarget
+{
+    GENERATED_BODY()
+
+public:
+    UGBFInteractableComponent();
+
+    virtual void GatherInteractionOptions( const FGBFInteractionQuery & interact_query, FGBFInteractionOptionBuilder & option_builder ) override;
+
+private:
+    UPROPERTY( EditAnywhere, BlueprintReadOnly, meta = ( AllowPrivateAccess = true ) )
+    FGBFInteractionOption Option;
+};
