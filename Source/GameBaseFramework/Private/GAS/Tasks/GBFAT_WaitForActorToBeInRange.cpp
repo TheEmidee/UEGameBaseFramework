cf82fd4f97e02a2058736a9caa8e282a0a00e114
Viktor Blomme <viktor.blomme@gmail.com>
2024-02-13 17:01:57 +0100

    Create wait actor in range task

---
+++ Source/GameBaseFramework/Private/GAS/Tasks/GBFAT_WaitForActorToBeInRange.cpp
@@ -0,0 +1,58 @@
+﻿#include "GAS/Tasks/GBFAT_WaitForActorToBeInRange.h"
+
+FGBFAxesToCheck::FGBFAxesToCheck() :
+    bCheckX( true ),
+    bCheckY( true ),
+    bCheckZ( true )
+{
+}
+
+UGBFAT_WaitForActorToBeInRange * UGBFAT_WaitForActorToBeInRange::WaitForActorToBeInRange( UGameplayAbility * owning_ability,
+    AActor * actor_to_wait_for,
+    const float range,
+    const FGBFAxesToCheck & axes_to_check,
+    const bool trigger_once )
+{
+    auto * my_obj = NewAbilityTask< UGBFAT_WaitForActorToBeInRange >( owning_ability );
+    my_obj->ActorToWaitFor = actor_to_wait_for;
+    my_obj->Range = range;
+    my_obj->AxesToCheck = axes_to_check;
+    my_obj->bTriggerOnce = trigger_once;
+    return my_obj;
+}
+
+void UGBFAT_WaitForActorToBeInRange::Activate()
+{
+    Super::Activate();
+
+    SetWaitingOnAvatar();
+
+    if ( GetAvatarActor() == nullptr || ActorToWaitFor == nullptr )
+    {
+        EndTask();
+    }
+}
+
+void UGBFAT_WaitForActorToBeInRange::TickTask( const float delta_time )
+{
+    Super::TickTask( delta_time );
+
+    const auto avatar_location = GetAvatarActor()->GetActorLocation();
+    const auto actor_location = ActorToWaitFor->GetActorLocation();
+
+    auto delta = avatar_location - actor_location;
+    delta.X *= AxesToCheck.bCheckX;
+    delta.Y *= AxesToCheck.bCheckY;
+    delta.Z *= AxesToCheck.bCheckZ;
+
+    const auto sq_distance = delta.SquaredLength();
+    if ( sq_distance <= Range * Range )
+    {
+        OnActorInRangeDelegate.Broadcast();
+
+        if ( bTriggerOnce )
+        {
+            EndTask();
+        }
+    }
+}
---
+++ Source/GameBaseFramework/Public/GAS/Tasks/GBFAT_WaitForActorToBeInRange.h
@@ -0,0 +1,51 @@
+﻿#pragma once
+
+#include <Abilities/Tasks/AbilityTask.h>
+#include <CoreMinimal.h>
+
+#include "GBFAT_WaitForActorToBeInRange.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE( FGBFOnActorInRangeDelegate );
+
+USTRUCT( BlueprintType )
+struct FGBFAxesToCheck
+{
+    GENERATED_BODY()
+
+    FGBFAxesToCheck();
+
+    UPROPERTY( EditDefaultsOnly, BlueprintReadWrite )
+    uint8 bCheckX : 1;
+
+    UPROPERTY( EditDefaultsOnly, BlueprintReadWrite )
+    uint8 bCheckY : 1;
+
+    UPROPERTY( EditDefaultsOnly, BlueprintReadWrite )
+    uint8 bCheckZ : 1;
+};
+
+UCLASS()
+class GAMEBASEFRAMEWORK_API UGBFAT_WaitForActorToBeInRange : public UAbilityTask
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION( BlueprintCallable, Category = "Ability|Tasks", meta = ( HidePin = "owning_ability", DefaultToSelf = "owning_ability", BlueprintInternalUseOnly = "TRUE" ) )
+    static UGBFAT_WaitForActorToBeInRange * WaitForActorToBeInRange( UGameplayAbility * owning_ability, AActor * actor_to_wait_for, float range, const FGBFAxesToCheck & axes_to_check, bool trigger_once );
+
+    void Activate() override;
+
+    void TickTask( float delta_time ) override;
+
+protected:
+    UPROPERTY( BlueprintAssignable )
+    FGBFOnActorInRangeDelegate OnActorInRangeDelegate;
+
+private:
+    UPROPERTY()
+    TObjectPtr< AActor > ActorToWaitFor;
+
+    float Range;
+    FGBFAxesToCheck AxesToCheck;
+    uint8 bTriggerOnce : 1;
+};
