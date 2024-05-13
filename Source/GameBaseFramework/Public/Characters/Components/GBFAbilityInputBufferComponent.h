261a2438133b872cd33b5d91ce44724f8994e0fb
Frédéric <frederic.deiber@fishingcactus.com>
2024-05-13 11:30:26 +0200

    remove uproperty and ufunction

--- Source/GameBaseFramework/Public/Characters/Components/GBFAbilityInputBufferComponent.h
+++ Source/GameBaseFramework/Public/Characters/Components/GBFAbilityInputBufferComponent.h
@@ -21,56 +21,35 @@ class GAMEBASEFRAMEWORK_API UGBFAbilityInputBufferComponent final : public UPawn
 public:
     UGBFAbilityInputBufferComponent( const FObjectInitializer & object_initializer );
     
-#if !UE_BUILD_SHIPPING
-    void TickComponent( float delta_time, ELevelTick tick_type, FActorComponentTickFunction * this_tick_function ) override;
-#endif
-    
     UFUNCTION( BlueprintCallable )
     void StartMonitoring( FGameplayTagContainer input_tags_to_check, ETriggerPriority trigger_priority );
 
     UFUNCTION( BlueprintCallable )
     void StopMonitoring();
+    
+#if !UE_BUILD_SHIPPING
+    void TickComponent( float delta_time, ELevelTick tick_type, FActorComponentTickFunction * this_tick_function ) override;
+#endif
 
 protected:
-    UFUNCTION()
-    void Reset();
-    
-    UFUNCTION()
-    void BindActions();
-    
-    UFUNCTION()
-    void RemoveBinds();
-    
-    UFUNCTION()
-    void AbilityInputTagPressed( FGameplayTag input_tag );
-    
-    UFUNCTION()
-    bool TryToTriggerAbility();
-
-    UFUNCTION()
-    FGameplayTag TryToGetInputTagWithPriority();
-    
-    UFUNCTION()
-    FGameplayTag GetLastTriggeredInput();
-    
-    UFUNCTION()
-    FGameplayTag GetMostTriggeredInput();
-
-    UPROPERTY()
-    ETriggerPriority TriggerPriority;
-    
-    UPROPERTY()
-    FGameplayTagContainer InputTagsToCheck;
-    
-    UPROPERTY()
-    TArray< FGameplayTag > TriggeredTags;
-    
-    UPROPERTY()
-    TArray< uint32 > BindHandles;
-
     UPROPERTY( EditDefaultsOnly )
     float MaxMonitoringTime = 5.0f;
     
-    UPROPERTY()
+    void Reset();
+    void BindActions();
+    void RemoveBinds();
+    void AbilityInputTagPressed( FGameplayTag input_tag );
+    bool TryToTriggerAbility();
+    
+    FGameplayTag TryToGetInputTagWithPriority();
+    FGameplayTag GetLastTriggeredInput();
+    FGameplayTag GetMostTriggeredInput();
+
+    ETriggerPriority TriggerPriority;
+    FGameplayTagContainer InputTagsToCheck;
+    
+    TArray< FGameplayTag > TriggeredTags;
+    TArray< uint32 > BindHandles;
+    
     float MonitoringTime = 0.0f;
 };
