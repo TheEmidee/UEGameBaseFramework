d54b2e3e18657796fb2c2b24308973bafcd87a58
Frédéric Deiber <frederic.deiber@gmail.com>
2024-05-28 11:02:49 +0200

    clang format

--- Source/GameBaseFramework/Private/Animation/GBFAnimationFunctionLibrary.cpp
+++ Source/GameBaseFramework/Private/Animation/GBFAnimationFunctionLibrary.cpp
@@ -2,7 +2,7 @@
 
 #include "Animation/GBFAnimLayerSelectionSet.h"
 
-TSubclassOf<UAnimInstance> UGBFAnimationFunctionLibrary::SelectBestLayerFromAnimLayerSelectionSet( const FGBFAnimLayerSelectionSet & anim_layer_selection_set, const FGameplayTagContainer & cosmetic_tags )
+TSubclassOf< UAnimInstance > UGBFAnimationFunctionLibrary::SelectBestLayerFromAnimLayerSelectionSet( const FGBFAnimLayerSelectionSet & anim_layer_selection_set, const FGameplayTagContainer & cosmetic_tags )
 {
     return anim_layer_selection_set.SelectBestLayer( cosmetic_tags );
 }
