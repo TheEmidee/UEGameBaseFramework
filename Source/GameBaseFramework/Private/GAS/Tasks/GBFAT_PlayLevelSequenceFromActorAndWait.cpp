a3306606b89ca62afaaf3d9866da98577300a02a
Frédéric <frederic.deiber@fishingcactus.com>
2024-08-09 15:17:19 +0200

    UGBFAT_PlayLevelSequenceFromActorAndWait

---
+++ Source/GameBaseFramework/Private/GAS/Tasks/GBFAT_PlayLevelSequenceFromActorAndWait.cpp
@@ -0,0 +1,36 @@
+#include "GAS/Tasks/GBFAT_PlayLevelSequenceFromActorAndWait.h"
+
+#include <LevelSequencePlayer.h>
+
+UGBFAT_PlayLevelSequenceFromActorAndWait * UGBFAT_PlayLevelSequenceFromActorAndWait::PlayLevelSequenceAndWait( UGameplayAbility * owning_ability, ALevelSequenceActor * level_sequence_actor, bool pause_at_end )
+{
+    auto * my_obj = NewAbilityTask< UGBFAT_PlayLevelSequenceFromActorAndWait >( owning_ability );
+    my_obj->LevelSequenceActor = level_sequence_actor;
+    my_obj->bPauseAtEnd = pause_at_end;
+    return my_obj;
+}
+
+void UGBFAT_PlayLevelSequenceFromActorAndWait::Activate()
+{
+    Super::Activate();
+
+    if ( LevelSequenceActor == nullptr )
+    {
+        return;
+    }
+
+    LevelSequenceActor->PlaybackSettings.bPauseAtEnd = bPauseAtEnd;
+    LevelSequenceActor->GetSequencePlayer()->OnFinished.AddDynamic( this, &UGBFAT_PlayLevelSequenceFromActorAndWait::OnSequenceEnded );
+    LevelSequenceActor->GetSequencePlayer()->Play();
+}
+
+void UGBFAT_PlayLevelSequenceFromActorAndWait::OnSequenceEnded()
+{
+    OnSequenceFinishedDelegate.Broadcast( LevelSequenceActor );
+}
+
+void UGBFAT_PlayLevelSequenceFromActorAndWait::OnDestroy( bool ability_ended )
+{
+    LevelSequenceActor->GetSequencePlayer()->OnFinished.RemoveDynamic( this, &UGBFAT_PlayLevelSequenceFromActorAndWait::OnSequenceEnded );
+    Super::OnDestroy( ability_ended );
+}
