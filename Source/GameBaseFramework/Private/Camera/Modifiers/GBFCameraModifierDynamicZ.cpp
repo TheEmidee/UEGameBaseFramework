7c25343cbd195b8d97ab3195e80cd06b238c8a42
Michael Delva <michael.delva@fishingcactus.com>
2024-08-22 13:08:31 +0200

    Clean up and fixes for GBFCameraModifierDynamicZ

--- Source/GameBaseFramework/Private/Camera/Modifiers/GBFCameraModifierDynamicZ.cpp
+++ Source/GameBaseFramework/Private/Camera/Modifiers/GBFCameraModifierDynamicZ.cpp
@@ -5,20 +5,12 @@
 #include <GameFramework/CharacterMovementComponent.h>
 
 UGBFCameraModifierDynamicZ::UGBFCameraModifierDynamicZ() :
-    bCorrectPitchWithVelocityZ( false ),
     LastGroundedPositionZ( 0.0f ),
-    CurrentPositionZ( 0.0f ),
     CurrentViewLocationZ( 0.0f ),
-    CurrentMovementMode( MOVE_None ),
+    TargetViewLocationZ( 0.0f ),
     CurrentInterpolationSpeed( 0.0f ),
     DeltaLastGroundedPositionZ( 0.0f ),
-    VelocityZ( 0.0f ),
-    PitchFromVelocityZ( 0.0f ),
-    PitchWhenGrounded( 0.0f ),
-    bPitchWasManuallyChanged( false ),
-    CurrentPitch( 0.0f ),
-    TimeSinceLanded( 0.0f ),
-    bJustLanded( false )
+    VelocityZ( 0.0f )
 {
 }
 
@@ -36,100 +28,38 @@ void UGBFCameraModifierDynamicZ::ModifyCamera( float delta_time, FVector view_lo
 {
     const auto * character = Cast< ACharacter >( GetViewTarget() );
 
-    const auto cmc_movement_mode = character->GetCharacterMovement()->MovementMode;
-
-    if ( CurrentMovementMode != cmc_movement_mode )
-    {
-        if ( cmc_movement_mode == MOVE_Walking && CurrentMovementMode == MOVE_Falling )
-        {
-            TimeSinceLanded = 0.0f;
-            bJustLanded = true;
-        }
-
-        CurrentMovementMode = cmc_movement_mode;
-    }
+    const auto current_movement_mode = character->GetCharacterMovement()->MovementMode;
 
     const auto character_z = character->GetActorLocation().Z;
     VelocityZ = character->GetCharacterMovement()->Velocity.Z;
 
-    if ( CurrentMovementMode == MOVE_Walking )
+    if ( current_movement_mode == MOVE_Walking )
     {
         LastGroundedPositionZ = character_z;
         CurrentViewLocationZ = view_location.Z;
-        PitchWhenGrounded = view_rotation.Pitch;
     }
     else
     {
-        DeltaLastGroundedPositionZ = FMath::Abs( character_z - LastGroundedPositionZ );
-        CurrentInterpolationSpeed = FMath::Abs( DistanceFromLastGroundedLocationInterpolationSpeedCurve.GetRichCurveConst()->Eval( VelocityZ ) );
+        DeltaLastGroundedPositionZ = character_z - LastGroundedPositionZ;
+        CurrentInterpolationSpeed = FMath::Abs( DistanceFromLastGroundedLocationInterpolationSpeedCurve.GetRichCurveConst()->Eval( DeltaLastGroundedPositionZ ) );
     }
 
-    const auto delta_current_view_z = view_location.Z - CurrentViewLocationZ;
-    const auto delta_sign = FMath::Sign( delta_current_view_z );
-    const auto velocity_sign = FMath::Sign( VelocityZ );
-
-    if ( delta_sign == velocity_sign )
+    if ( current_movement_mode == MOVE_Falling )
     {
-        CurrentViewLocationZ = FMath::FInterpTo( CurrentViewLocationZ, view_location.Z, delta_time, CurrentInterpolationSpeed );
+        TargetViewLocationZ = view_location.Z;
+        CurrentViewLocationZ = FMath::FInterpTo( CurrentViewLocationZ, TargetViewLocationZ, delta_time, CurrentInterpolationSpeed );
+        new_view_location.Z = CurrentViewLocationZ;
     }
-
-    new_view_location.Z = CurrentViewLocationZ;
-
-    if ( bCorrectPitchWithVelocityZ )
-    {
-        bool should_update_pitch = false;
-
-        if ( VelocityZ < -600.0f )
-        {
-            PitchFromVelocityZ = VelocityZToPitchCurve.GetRichCurveConst()->Eval( VelocityZ );
-            should_update_pitch = true;
-        }
-        else if ( bJustLanded )
-        {
-            TimeSinceLanded += delta_time;
-
-            if ( TimeSinceLanded > 1.0f )
-            {
-                bJustLanded = false;
-            }
-            else
-            {
-                PitchFromVelocityZ = PitchWhenGrounded;
-                should_update_pitch = true;
-            }
-        }
-        else
-        {
-            PitchFromVelocityZ = view_rotation.Pitch;
-        }
-
-        if ( should_update_pitch )
-        {
-            CurrentPitch = FMath::FInterpTo( CurrentPitch, PitchFromVelocityZ, delta_time, 2.0f );
-            new_view_rotation.Pitch = CurrentPitch;
-        }
-    }
-}
-
-bool UGBFCameraModifierDynamicZ::ProcessViewRotation( AActor * ViewTarget, float DeltaTime, FRotator & OutViewRotation, FRotator & OutDeltaRot )
-{
-    return false;
 }
 
 void UGBFCameraModifierDynamicZ::DisplayDebugInternal( UCanvas * canvas, const FDebugDisplayInfo & debug_display, float & yl, float & y_pos ) const
 {
     auto & display_debug_manager = canvas->DisplayDebugManager;
 
-    display_debug_manager.DrawString( FString::Printf( TEXT( "CurrentMovementMode: %s" ), *UEnum::GetValueAsString( CurrentMovementMode ) ) );
     display_debug_manager.DrawString( FString::Printf( TEXT( "LastGroundedPositionZ: %s" ), *FString::SanitizeFloat( LastGroundedPositionZ ) ) );
     display_debug_manager.DrawString( FString::Printf( TEXT( "DeltaLastGroundedPositionZ: %s" ), *FString::SanitizeFloat( DeltaLastGroundedPositionZ ) ) );
     display_debug_manager.DrawString( FString::Printf( TEXT( "VelocityZ: %s" ), *FString::SanitizeFloat( VelocityZ ) ) );
     display_debug_manager.DrawString( FString::Printf( TEXT( "CurrentInterpolationSpeed: %s" ), *FString::SanitizeFloat( CurrentInterpolationSpeed ) ) );
     display_debug_manager.DrawString( FString::Printf( TEXT( "CurrentViewLocationZ: %s" ), *FString::SanitizeFloat( CurrentViewLocationZ ) ) );
-
-    if ( bCorrectPitchWithVelocityZ )
-    {
-        display_debug_manager.DrawString( FString::Printf( TEXT( "PitchFromVelocityZ: %s" ), *FString::SanitizeFloat( PitchFromVelocityZ ) ) );
-        display_debug_manager.DrawString( FString::Printf( TEXT( "CurrentPitch: %s" ), *FString::SanitizeFloat( PitchFromVelocityZ ) ) );
-    }
+    display_debug_manager.DrawString( FString::Printf( TEXT( "TargetViewLocationZ: %s" ), *FString::SanitizeFloat( TargetViewLocationZ ) ) );
 }
--- Source/GameBaseFramework/Public/Camera/Modifiers/GBFCameraModifierDynamicZ.h
+++ Source/GameBaseFramework/Public/Camera/Modifiers/GBFCameraModifierDynamicZ.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "Camera/Modifiers/GBFCameraModifier.h"
+#include "GBFCameraModifier.h"
 
 #include <CoreMinimal.h>
 #include <Curves/CurveFloat.h>
@@ -19,34 +19,16 @@ public:
 
 protected:
     void ModifyCamera( float delta_time, FVector view_location, FRotator view_rotation, float fov, FVector & new_view_location, FRotator & new_view_rotation, float & new_fov ) override;
-
-public:
-    bool ProcessViewRotation( AActor * ViewTarget, float DeltaTime, FRotator & OutViewRotation, FRotator & OutDeltaRot ) override;
-
-protected:
     void DisplayDebugInternal( UCanvas * canvas, const FDebugDisplayInfo & debug_display, float & yl, float & y_pos ) const override;
 
 private:
     UPROPERTY( EditAnywhere )
     FRuntimeFloatCurve DistanceFromLastGroundedLocationInterpolationSpeedCurve;
 
-    UPROPERTY( EditAnywhere, meta = ( InlineEditConditionToggle ) )
-    uint8 bCorrectPitchWithVelocityZ : 1;
-
-    UPROPERTY( EditAnywhere, meta = ( EditCondition = "bCorrectPitchWithVelocityZ" ) )
-    FRuntimeFloatCurve VelocityZToPitchCurve;
-
     float LastGroundedPositionZ;
-    float CurrentPositionZ;
     float CurrentViewLocationZ;
-    EMovementMode CurrentMovementMode;
+    float TargetViewLocationZ;
     float CurrentInterpolationSpeed;
     float DeltaLastGroundedPositionZ;
     float VelocityZ;
-    float PitchFromVelocityZ;
-    float PitchWhenGrounded;
-    bool bPitchWasManuallyChanged;
-    float CurrentPitch;
-    float TimeSinceLanded;
-    bool bJustLanded;
 };
