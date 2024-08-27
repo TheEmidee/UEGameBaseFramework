a55c6d63b855a403f229195ae254e8fc606d5202
Michael Delva <michael@emidee.net>
2024-08-27 09:26:46 +0200

    clang-format

--- Source/GameBaseFramework/Private/Camera/Modifiers/GBFCameraModifierUtils.cpp
+++ Source/GameBaseFramework/Private/Camera/Modifiers/GBFCameraModifierUtils.cpp
@@ -25,7 +25,7 @@ bool FGBFCameraModifierUtilsLibrary::LineTraceSingleByChannel( AActor * view_tar
     return UKismetSystemLibrary::LineTraceSingle( view_target, start, end, object_type_query, false, { view_target }, draw_debug_type, hit_result, false );
 }
 
-bool FGBFCameraModifierUtilsLibrary::SphereTraceSingleByChannel(AActor * view_target, FHitResult & hit_result, const FVector & start, const FVector & end, float radius, const TEnumAsByte<ECollisionChannel> collision_channel, bool draw_debug)
+bool FGBFCameraModifierUtilsLibrary::SphereTraceSingleByChannel( AActor * view_target, FHitResult & hit_result, const FVector & start, const FVector & end, float radius, const TEnumAsByte< ECollisionChannel > collision_channel, bool draw_debug )
 {
     const auto draw_debug_type = draw_debug ? EDrawDebugTrace::ForOneFrame : EDrawDebugTrace::None;
     const auto object_type_query = UCollisionProfile::Get()->ConvertToTraceType( collision_channel );
