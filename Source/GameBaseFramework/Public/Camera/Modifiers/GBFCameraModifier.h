1d0f1bfd39a70ba260279821e5556019f7cc82d6
Frédéric <frederic.deiber@fishingcactus.com>
2024-09-26 17:22:08 +0200

    clang

--- Source/GameBaseFramework/Public/Camera/Modifiers/GBFCameraModifier.h
+++ Source/GameBaseFramework/Public/Camera/Modifiers/GBFCameraModifier.h
@@ -15,15 +15,15 @@ class GAMEBASEFRAMEWORK_API UGBFCameraModifier : public UCameraModifier
 
 public:
     friend class AGBFPlayerCameraManager;
-    
+
     bool IsDisabled() const override;
     void AddedToCamera( APlayerCameraManager * player_camera_manager ) override;
     virtual void OnViewTargetChanged( AActor * view_target );
     virtual void RemovedFromCameraManager();
     void DisplayDebug( UCanvas * canvas, const FDebugDisplayInfo & debug_display, float & yl, float & y_pos ) final;
-    
 
-        protected : virtual void DisplayDebugInternal( UCanvas * canvas, const FDebugDisplayInfo & debug_display, float & yl, float & y_pos ) const;
+protected:
+    virtual void DisplayDebugInternal( UCanvas * canvas, const FDebugDisplayInfo & debug_display, float & yl, float & y_pos ) const;
 
     UFUNCTION( BlueprintImplementableEvent )
     void ReceiveAddedToCameraManager( APlayerCameraManager * camera_manager );
@@ -33,7 +33,7 @@ public:
 
 private:
     bool ModifyCameraForOwner( APlayerCameraManager * camera_owner, float delta_time, FMinimalViewInfo & in_out_pov );
-    
+
     UPROPERTY( EditDefaultsOnly )
     FGameplayTagRequirements TagRequirements;
 };
