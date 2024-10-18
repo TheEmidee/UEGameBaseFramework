2f25eb7848ab8cfe31c7f20836087256c8d02e25
Michael Delva <michael@emidee.net>
2024-10-18 21:08:39 +0200

    Removed GBFInteractableTarget and updated all the interaction code to only use GBFInteractableComponent

--- Source/GameBaseFramework/Public/Interaction/GBFInteractableComponent.h
+++ Source/GameBaseFramework/Public/Interaction/GBFInteractableComponent.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
 
-#include "GBFInteractableTarget.h"
+#include "GBFInteractionOption.h"
 
 #include <Components/ActorComponent.h>
 #include <CoreMinimal.h>
@@ -12,20 +12,23 @@ class UInputMappingContext;
 class UGBFInputConfig;
 
 UCLASS( Blueprintable, meta = ( BlueprintSpawnableComponent ) )
-class GAMEBASEFRAMEWORK_API UGBFInteractableComponent : public UActorComponent, public IGBFInteractableTarget
+class GAMEBASEFRAMEWORK_API UGBFInteractableComponent : public UActorComponent
 {
     GENERATED_BODY()
 
 public:
     UGBFInteractableComponent();
 
-    const FGBFInteractionOptionContainer & GetInteractableOptions() const override;
+    const FGBFInteractionOptionContainer & GetInteractableOptions() const;
 
     UFUNCTION( BlueprintCallable )
     void UpdateInteractions( UGBFInteractionOptionsData * options_data );
 
     UFUNCTION( BlueprintCallable )
     void RemoveInteractions();
+
+    UFUNCTION( BlueprintNativeEvent, meta = ( ForceAsFunction ) )
+    void CustomizeInteractionEventData( UPARAM( ref ) FGameplayEventData & event_data, FGameplayTag event_tag );
 
 private:
     UPROPERTY( EditDefaultsOnly, BlueprintReadWrite, meta = ( AllowPrivateAccess = true ) )
