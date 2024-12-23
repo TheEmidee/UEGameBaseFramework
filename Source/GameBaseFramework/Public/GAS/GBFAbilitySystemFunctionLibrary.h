3982b81a44f0ad04fec0fb6b881fb54a0c3ded7f
Dimitri Paccagnini <dimitri.dimitri@fishingcactus.com>
2024-12-23 12:36:54 +0100

    changed Category

--- Source/GameBaseFramework/Public/GAS/GBFAbilitySystemFunctionLibrary.h
+++ Source/GameBaseFramework/Public/GAS/GBFAbilitySystemFunctionLibrary.h
@@ -70,7 +70,7 @@ public:
     UFUNCTION( BlueprintPure, Category = "Ability|GameplayEffects" )
     static TSubclassOf< UGameplayEffect > GetGameplayEffectClassFromSpecHandle( FGameplayEffectSpecHandle spec_handle );
     
-    UFUNCTION( BlueprintCallable, Category = "Core|GameplayTags" )
+    UFUNCTION( BlueprintCallable, Category = "Ability|GameplayTags" )
     static bool AreGameplayTagRequirementsMet( const FGameplayTagRequirements & requirements, const FGameplayTagContainer & container );
 
     static void CopySetByCallerTagMagnitudesFromSpecToConditionalEffects( FGameplayEffectSpec * gameplay_effect_spec );
