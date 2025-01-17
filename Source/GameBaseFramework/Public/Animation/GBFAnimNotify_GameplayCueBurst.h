d8ba8dabb615dfc7b540b4132a60e244d3b31763
Thibaut Spreux <thibaut.spreux@fishingcactus.com>
2025-01-17 11:33:22 +0100

    anim notify burst gc

---
+++ Source/GameBaseFramework/Public/Animation/GBFAnimNotify_GameplayCueBurst.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <Animation/AnimNotifies/AnimNotify.h>
+#include <CoreMinimal.h>
+#include <GAS/GameplayCues/GBFGameplayCueManager.h>
+
+#include "GBFAnimNotify_GameplayCueBurst.generated.h"
+
+UCLASS()
+class GAMEBASEFRAMEWORK_API UGBFAnimNotify_GameplayCueBurst : public UAnimNotify
+{
+    GENERATED_BODY()
+
+public:
+    UGBFAnimNotify_GameplayCueBurst() = default;
+
+    void Notify( USkeletalMeshComponent * mesh_component, UAnimSequenceBase * animation, const FAnimNotifyEventReference & event_reference ) override;
+
+private:
+    UPROPERTY( EditAnywhere, Category = "GameplayCue", meta = ( Categories = "GameplayCue", AllowPrivateAccess ) )
+    FGameplayTag GameplayCueTag;
+
+    UPROPERTY( EditAnywhere, Category = "GameplayCue", meta = ( Categories = "GameplayCue", AllowPrivateAccess ) )
+    FGameplayCueParameters Parameters;
+};
