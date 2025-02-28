eb6949298aaf262a31d0c1612d24e95744045397
Viktor Blomme <viktor.blomme@gmail.com>
2025-02-28 17:19:45 +0100

    Add new methods to init the player at start completely

--- Source/GameBaseFramework/Private/Chaos/GBFChaosCachePlayer.cpp
+++ Source/GameBaseFramework/Private/Chaos/GBFChaosCachePlayer.cpp
@@ -26,10 +26,50 @@ void AGBFChaosCachePlayer::AddNewObservedComponentFromCacheCollectionIndex( UPri
     BeginEvaluate();
 }
 
+void AGBFChaosCachePlayer::AddAllObservedComponentsFromCacheCollection( UPrimitiveComponent * observed_component )
+{
+    if ( !ensureMsgf( observed_component != nullptr, TEXT( "Observed component is not valid!" ) ) )
+    {
+        return;
+    }
+
+    if ( !ensureMsgf( CacheCollection != nullptr, TEXT( "CacheCollection is not valid!" ) ) )
+    {
+        return;
+    }
+
+    auto & caches = CacheCollection->GetCaches();
+
+    for ( auto & cache : caches )
+    {
+        auto & new_observed_component = AddNewObservedComponent( observed_component );
+        new_observed_component.CacheName = cache->GetFName();
+    }
+}
+
+void AGBFChaosCachePlayer::TriggerComponentByCacheCollectionIndex( const int32 collection_index )
+{
+    auto & caches = CacheCollection->GetCaches();
+
+    if ( !ensureMsgf( caches.IsValidIndex( collection_index ), TEXT( "Index %i is not a valid index in cache collection %s" ), collection_index, *CacheCollection.GetFName().ToString() ) )
+    {
+        return;
+    }
+
+    const auto cache_name = caches[ collection_index ]->GetFName();
+
+    TriggerComponentByCache( cache_name );
+}
+
 void AGBFChaosCachePlayer::ResetObservedComponents()
 {
     EndEvaluate();
     SetStartTime( 0.0f );
+    BeginEvaluate();
+}
+
+void AGBFChaosCachePlayer::K2_BeginEvaluate()
+{
     BeginEvaluate();
 }
 
