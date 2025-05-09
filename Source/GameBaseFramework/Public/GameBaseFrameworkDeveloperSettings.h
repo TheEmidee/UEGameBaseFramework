6b7360e9b541e519bbe496d9dc04739c128cf6f0
Michael Delva <michael.delva@fishingcactus.com>
2025-05-09 14:55:49 +0200

    Removed default experience in the settings

--- Source/GameBaseFramework/Public/GameBaseFrameworkDeveloperSettings.h
+++ Source/GameBaseFramework/Public/GameBaseFrameworkDeveloperSettings.h
@@ -31,9 +31,6 @@ public:
     UPROPERTY( config, EditAnywhere, Category = "Cheats" )
     TArray< FGBFCheatToRun > CheatsToRun;
 
-    UPROPERTY( EditDefaultsOnly, BlueprintReadOnly, config, Category = "Experience", meta = ( AllowedTypes = "ExperienceDefinition" ) )
-    FPrimaryAssetId DefaultExperience;
-
     // The experience override to use for Play in Editor (if not set, the default for the world settings of the open map will be used)
     UPROPERTY( EditDefaultsOnly, BlueprintReadOnly, config, Category = "Experience", meta = ( AllowedTypes = "ExperienceDefinition" ) )
     FPrimaryAssetId ExperienceOverride;
