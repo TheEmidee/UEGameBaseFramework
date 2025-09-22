3a07596af5731a0a5c7471c2c10d449cbd7e07a1
Viktor Blomme <viktor.blomme@fishingcactus.com>
2025-09-22 15:10:44 +0200

    Fix no super called

--- Source/UIExtension/Source/Private/UIExtensionSystem.cpp
+++ Source/UIExtension/Source/Private/UIExtensionSystem.cpp
@@ -62,6 +62,8 @@ bool FUIExtensionPoint::DoesExtensionPassContract(const FUIExtension* Extension)
 
 void UUIExtensionSubsystem::AddReferencedObjects(UObject* InThis, FReferenceCollector& Collector)
 {
+    Super::AddReferencedObjects( InThis, Collector );
+    
 	if (UUIExtensionSubsystem* ExtensionSubsystem = Cast<UUIExtensionSubsystem>(InThis))
 	{
 		for (auto MapIt = ExtensionSubsystem->ExtensionPointMap.CreateIterator(); MapIt; ++MapIt)
